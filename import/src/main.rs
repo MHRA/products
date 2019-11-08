8f61116a7b824889f1275cf5426cb46aa6119c74
Stuart Harris <stuart.harris@red-badger.com>
2019-11-08 14:19:19 +0000

    make import into cli

--- import/src/main.rs
+++ import/src/main.rs
@@ -1,14 +1,26 @@
+#[macro_use]
+extern crate clap;
+
 use azure_sdk_core::errors::AzureError;
 use azure_sdk_storage_core::prelude::*;
+use clap::App;
+use std::fs;
 use tokio_core::reactor::Core;
 
 fn main() -> Result<(), AzureError> {
-    let (client, mut core) = initialize()?;
-    spc_pil_import::upload(
-        client,
-        &mut core,
-        include_bytes!("../tests/fixtures/MHRA MIP HLD v0.1.pdf"),
-    )
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+    Ok(match matches.subcommand_matches("pdf") {
+        Some(pdf_matches) => {
+            let path = pdf_matches
+                .value_of("file")
+                .expect("yaml is incorrect: file should be a required arg");
+            println!("importing {}", path);
+            let (client, mut core) = initialize()?;
+            import::upload(client, &mut core, &fs::read(path)?)?
+        }
+        None => println!("yaml is incorrect: pdf is currently the only subcommand"),
+    })
 }
 
 fn initialize() -> Result<(Client, Core), AzureError> {
@@ -16,6 +28,7 @@ fn initialize() -> Result<(Client, Core), AzureError> {
         std::env::var("STORAGE_ACCOUNT").expect("Set env variable STORAGE_ACCOUNT first!");
     let master_key =
         std::env::var("STORAGE_MASTER_KEY").expect("Set env variable STORAGE_MASTER_KEY first!");
+
     let core = Core::new()?;
 
     Ok((Client::new(&account, &master_key)?, core))
--- import/tests/hash_test.rs
+++ import/tests/hash_test.rs
@@ -1,10 +1,10 @@
-use spc_pil_import;
+use import;
 
 #[test]
 fn test_hash() {
     let bytes = include_bytes!("fixtures/MHRA MIP HLD v0.1.pdf");
     assert_eq!(
         "03e1b22894bd2d3bd087402367147fce9e50ffea",
-        spc_pil_import::hash(bytes)
+        import::hash(bytes)
     );
 }
