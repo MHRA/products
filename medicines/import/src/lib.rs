bba2280db975f8c764d7fa72d68810761b5a3dc6
Stuart Harris <stuart.harris@red-badger.com>
2019-11-20 13:46:32 +0000

    Import with metadata (#3)
    * import with doc_type
    
    * remove unneccessary type annotation
    
    * remove unused imports

--- medicines/import/src/lib.rs
+++ medicines/import/src/lib.rs
@@ -8,7 +8,16 @@ use azure_sdk_storage_blob::{
 };
 use azure_sdk_storage_core::prelude::*;
 use sha1;
+use std::collections::HashMap;
 use tokio_core::reactor::Core;
+
+#[derive(Debug)]
+pub enum DocType {
+    PilLabel,
+    PilLabelAndLeaflet,
+    PilLeaflet,
+    Spc,
+}
 
 pub fn hash(bytes: &[u8]) -> String {
     let mut m = sha1::Sha1::new();
@@ -16,9 +25,14 @@ pub fn hash(bytes: &[u8]) -> String {
     m.digest().to_string()
 }
 
-pub fn upload(client: Client, core: &mut Core, data: &[u8]) -> Result<(), AzureError> {
+pub fn upload(
+    client: &Client,
+    core: &mut Core,
+    data: &[u8],
+    doc_type: DocType,
+) -> Result<(), AzureError> {
     let blob_name = hash(data);
-    let container_name: &'static str = "tmp";
+    let container_name = "docs";
 
     if core
         .run(client.list_containers().finalize())?
@@ -38,12 +52,15 @@ pub fn upload(client: Client, core: &mut Core, data: &[u8]) -> Result<(), AzureE
 
     // calculate md5 too!
     let digest = md5::compute(&data[..]);
-
+    let mut metadata = HashMap::new();
+    let d = format!("{:?}", &doc_type);
+    metadata.insert("doc_type", d.as_str());
     let future = client
         .put_block_blob()
         .with_container_name(&container_name)
         .with_blob_name(&blob_name)
-        .with_content_type("text/plain")
+        .with_content_type("application/pdf")
+        .with_metadata(&metadata)
         .with_body(&data[..])
         .with_content_md5(&digest[..])
         .finalize();
--- medicines/import/src/main.rs
+++ medicines/import/src/main.rs
@@ -4,23 +4,57 @@ extern crate clap;
 use azure_sdk_core::errors::AzureError;
 use azure_sdk_storage_core::prelude::*;
 use clap::App;
-use std::fs;
+use import::DocType;
+use std::{fs, path::Path};
 use tokio_core::reactor::Core;
 
 fn main() -> Result<(), AzureError> {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml).get_matches();
-    Ok(match matches.subcommand_matches("pdf") {
+    match matches.subcommand_matches("pdf") {
         Some(pdf_matches) => {
             let path = pdf_matches
-                .value_of("file")
-                .expect("yaml is incorrect: file should be a required arg");
-            println!("importing {}", path);
+                .value_of("directory")
+                .expect("yaml is incorrect: directory should be a required arg");
             let (client, mut core) = initialize()?;
-            import::upload(client, &mut core, &fs::read(path)?)?
+            let dir = Path::new(&path);
+            if dir.is_dir() {
+                for entry in fs::read_dir(dir)? {
+                    let entry = entry?;
+                    let path = entry.path();
+                    if let Some(ext) = path.extension() {
+                        if ext == "pdf" && fs::metadata(&path)?.len() > 0 {
+                            if let Some(doc_type) = get_doc_type(
+                                Path::file_stem(&path)
+                                    .expect("no file stem")
+                                    .to_str()
+                                    .expect("cannot convert OSStr to str"),
+                            ) {
+                                println!("{:?} {:?}", path, doc_type);
+                                import::upload(&client, &mut core, &fs::read(path)?, doc_type)?
+                            }
+                        }
+                    }
+                }
+            }
         }
         None => println!("yaml is incorrect: pdf is currently the only subcommand"),
-    })
+    };
+    Ok(())
+}
+
+fn get_doc_type(file_stem: &str) -> Option<DocType> {
+    if file_stem.starts_with("label-and-leaflet") {
+        Some(DocType::PilLabelAndLeaflet)
+    } else if file_stem.starts_with("label") {
+        Some(DocType::PilLabel)
+    } else if file_stem.starts_with("leaflet") {
+        Some(DocType::PilLeaflet)
+    } else if file_stem.starts_with("spc") {
+        Some(DocType::Spc)
+    } else {
+        None
+    }
 }
 
 fn initialize() -> Result<(Client, Core), AzureError> {
