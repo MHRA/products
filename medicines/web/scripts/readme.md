2b4bda6174e4990cb37d50a5ebf11e48309d4ad6
Pedro Martin <pedro.martin@red-badger.com>
2019-11-20 11:41:24 +0000

    Style header

---
+++ medicines/web/scripts/readme.md
@@ -0,0 +1,9 @@
+# Scripts
+
+## SVGR
+
+ <!-- TODO: Write doc for SVGR exporter -->
+
+```sh
+ npx @svgr/cli --template svgr.js --ext .ts ./src/assets/images/yellow-card.svg > ./src/components/logos/yellow-card.tsx
+```
---
+++ medicines/web/src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const black = '#000';
+export const white = '#fff';
+export const mhraBlue = 'rgb(15, 18, 144)';
+export const mhraBlue90 = 'rgba(15, 18, 144, 0.9)';
+export const mhraYellow = 'rgb(252, 217, 0)';
+export const mhraGray = 'rgb(154, 155, 156)';
+export const mhraGray30 = 'rgba(154, 155, 156, 0.3)';
+
+export const primaryColor = mhraBlue;
