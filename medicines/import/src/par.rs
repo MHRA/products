e4f93fe60c3d329588ff8e8bf341f730caf2f860
Stuart Harris <stuart.harris@red-badger.com>
2019-11-22 11:40:52 +0000

    parse csv into hashtable of records

--- medicines/import/src/par.rs
+++ medicines/import/src/par.rs
@@ -1,8 +1,42 @@
 use azure_sdk_core::errors::AzureError;
 use azure_sdk_storage_core::prelude::*;
-use std::path::Path;
+use csv;
+use std::{
+    collections::HashMap,
+    fs,
+    fs::{DirEntry, File},
+    io::BufReader,
+    path::Path,
+};
 use tokio_core::reactor::Core;
 
-pub fn import(_dir: &Path, _client: Client, mut _core: Core) -> Result<(), AzureError> {
+#[derive(Debug, Deserialize, Clone)]
+struct Record {
+    #[serde(rename = "dDocAuthor")]
+    author: String,
+    #[serde(rename = "dDocName")]
+    filename: String,
+}
+
+pub fn import(dir: &Path, _client: Client, mut _core: Core) -> Result<(), AzureError> {
+    if dir.is_dir() {
+        if let Some(Ok(f)) = fs::read_dir(dir)?.find(|f| is_csv(f.as_ref().unwrap())) {
+            println!("Found CSV file: {:?}", f);
+            let file = File::open(&f.path())?;
+            let mut rdr = csv::Reader::from_reader(BufReader::new(file));
+            let records = rdr
+                .deserialize()
+                .map(|r: Result<Record, csv::Error>| {
+                    let r = r.unwrap();
+                    (r.filename.clone().to_lowercase(), r)
+                })
+                .collect::<HashMap<String, Record>>();
+            println!("{:?}", records);
+        }
+    }
     Ok(())
+}
+
+fn is_csv(f: &DirEntry) -> bool {
+    "csv" == f.path().extension().unwrap_or_default()
 }
