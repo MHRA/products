19965163f2fa9d56f29aeb12a6a96608f67d5d44
Stuart Harris <stuart.harris@red-badger.com>
2019-11-23 19:44:28 +0000

    initial attempt using tantivy to tokenise keywords

--- medicines/import/src/par.rs
+++ medicines/import/src/par.rs
@@ -12,7 +12,6 @@ use std::{
     str,
 };
 use tokio_core::reactor::Core;
-use urlencoding;
 
 #[derive(Debug, Deserialize, Clone)]
 struct Record {
@@ -55,9 +54,9 @@ pub fn import(dir: &Path, client: Client, mut core: Core) -> Result<(), AzureErr
                             let mut metadata = HashMap::new();
                             let d = format!("{:?}", model::DocType::Par);
                             let created = record.created.to_string();
-                            let title = urlencoding::encode(&record.title);
-                            let author = urlencoding::encode(&record.author);
-                            let keywords = urlencoding::encode(&record.keywords);
+                            let title = sanitize(record.title.to_string());
+                            let author = sanitize(record.author.to_string());
+                            let keywords = sanitize(record.keywords.to_string());
                             metadata.insert("doc_type", d.as_str());
                             metadata.insert("file_name", &record.filename);
                             metadata.insert("title", &title);
@@ -77,4 +76,45 @@ pub fn import(dir: &Path, client: Client, mut core: Core) -> Result<(), AzureErr
 
 fn is_csv(f: &DirEntry) -> bool {
     "csv" == f.path().extension().unwrap_or_default()
+}
+
+use tantivy::tokenizer::*;
+
+fn sanitize(s: String) -> String {
+    let en_stem = SimpleTokenizer
+        .filter(RemoveLongFilter::limit(40))
+        .filter(LowerCaser)
+        .filter(StopWordFilter::default());
+    let mut tokens: Vec<Token> = vec![];
+    {
+        let mut add_token = |token: &Token| {
+            tokens.push(token.clone());
+        };
+        en_stem.token_stream(&s).process(&mut add_token);
+    }
+    tokens
+        .iter()
+        .map(|t| t.text.to_string())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn remove_newline() {
+        assert_eq!(sanitize("newline\ntest".to_string()), "newline test");
+    }
+    #[test]
+    fn remove_non_ascii() {
+        assert_eq!(sanitize("emoji🙂test".to_string()), "emoji test");
+    }
+    #[test]
+    fn sample_keywords1() {
+        let s1 = "ukpar, public assessment report, par, national procedure,Ibuprofen, Phenylephrine Hydrochloride, Ibuprofen and Phenylephrine Hydrochloride 200 mg/6.1 mg Tablets, 200 mg, 6.1 mg, cold, flu, congestion, aches, pains, headache, fever, sore throat, blocked nose, sinuses";
+        let s2 = "ukpar public assessment report par national procedure ibuprofen phenylephrine hydrochloride ibuprofen phenylephrine hydrochloride 200 mg 6 1 mg tablets 200 mg 6 1 mg cold flu congestion aches pains headache fever sore throat blocked nose sinuses";
+        assert_eq!(sanitize(s1.to_string()), s2.to_string());
+    }
 }
