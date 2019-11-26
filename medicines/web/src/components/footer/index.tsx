0a39c610ba08d3d960b9b578e408f30448360ee5
Pedro Martin <pedro.martin@red-badger.com>
2019-11-26 14:32:12 +0000

    Style footer

--- medicines/web/src/components/footer/index.tsx
+++ medicines/web/src/components/footer/index.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import { black, mhraGray20 } from '../../styles/colors';
 import { baseSpace, desktopMaxWidth } from '../../styles/dimensions';
 import SvgAgencyDigitalLogo from '../logos/agency-digital-logo';
 import SvgCrownGrey from '../logos/crown-grey';
 
 const StyledFooter = styled.footer`
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-  padding: ${baseSpace};
-  margin: 0 auto;
+  background-color: ${mhraGray20};
+  padding: ${baseSpace} ${baseSpace} ${baseSpace} 3.75rem;
   max-width: ${desktopMaxWidth};
+  margin: 0 auto;
 
   picture {
-    flex-basis: 25%;
     max-width: 250px;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+  }
+  li {
+    padding-right: 1.875rem;
+  }
+
+  a {
+    color: ${black};
   }
 `;
 
@@ -23,9 +35,26 @@ const Footer: React.FC = () => (
     <picture>
       <SvgAgencyDigitalLogo />
     </picture>
-    <picture>
-      <SvgCrownGrey />
-    </picture>
+    <nav>
+      <ul>
+        <li>
+          <p>
+            <a href="">Cookie Policy</a>
+          </p>
+        </li>
+        <li>
+          <p>
+            <a href="">Privacy Policy</a>
+          </p>
+        </li>
+        <li>
+          <p>
+            Built by the
+            <a href="">Medicines &amp; Healthcare products Regulatory Agency</a>
+          </p>
+        </li>
+      </ul>
+    </nav>
   </StyledFooter>
 );
 
