5cf6a4442c669c5ea1003be3895bb74d590fa207
Pedro Martin <pedro.martin@red-badger.com>
2019-11-28 14:53:06 +0000

    Enable env variables on NextJS

--- medicines/web/next.config.js
+++ medicines/web/next.config.js
@@ -1,6 +1,12 @@
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   distDir: 'dist',
   poweredByHeader: false,
+  webpack: config => {
+    config.plugins.push(new Dotenv());
+    return config;
+  },
   // exportPathMap: function() {
   //   return {
   //     '/': { page: '/' },
