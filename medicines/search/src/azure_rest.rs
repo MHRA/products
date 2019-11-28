7bb46dc4cfb93eb73f43bd10a8ae1e1d30e25de0
Matt Doughty <matt.doughty@mhra.gov.uk>
2019-11-28 17:30:03 +0000

    Remove and_then from http client call.

--- medicines/search/src/azure_rest.rs
+++ medicines/search/src/azure_rest.rs
@@ -3,19 +3,18 @@ use actix_web::client::{Client, SendRequestError};
 use std::time::Duration;
 
 pub fn send_json_post_request(definition: String, url: &str, api_key: &str) -> Result<(), SendRequestError> {
-    System::new("create_indexer").block_on(async {
+    System::new("azure_rest").block_on(async {
         let client = Client::default();
 
-        client
+        let response = client
             .post(url)
             .set(actix_web::http::header::ContentType(mime::APPLICATION_JSON))
             .header("api-key", api_key)
             .timeout(Duration::new(3600, 0))
             .send_body(definition)
-            .await
-            .and_then(|response| {
-                println!("{:#?}", response);
-                Ok(())
-            })
+            .await?;
+
+        println!("{:#?}", response);
+        Ok(())
     })
 }
