b2f21d67e1fe4e642c6add5e44f9ea5f587bb1ef
Stuart Harris <stuart.harris@red-badger.com>
2019-11-29 12:53:10 +0000

    upload spc/pil with metadata

--- medicines/import/src/spc_pil.rs
+++ medicines/import/src/spc_pil.rs
@@ -1,45 +1,54 @@
-use crate::{model, storage};
+use crate::{csv, metadata, model, pdf, storage};
 use azure_sdk_core::errors::AzureError;
 use azure_sdk_storage_core::prelude::*;
-use std::{collections::HashMap, fs, path::Path};
+use std::{collections::HashMap, fs, path::Path, str};
 use tokio_core::reactor::Core;
 
 pub fn import(dir: &Path, client: Client, mut core: Core) -> Result<(), AzureError> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if let Some(ext) = path.extension() {
-                if ext == "pdf" && fs::metadata(&path)?.len() > 0 {
-                    if let Some(doc_type) = get_doc_type(
-                        Path::file_stem(&path)
-                            .expect("no file stem")
-                            .to_str()
-                            .expect("cannot convert OSStr to str"),
-                    ) {
-                        println!("{:?} {:?}", path, doc_type);
-                        let mut metadata: HashMap<&str, &str> = HashMap::new();
-                        let doc_type = format!("{:?}", &doc_type);
-                        metadata.insert("doc_type", &doc_type);
-                        storage::upload(&client, &mut core, &fs::read(path)?, &metadata)?
+    if let Ok(records) = csv::load_csv(dir) {
+        for path in pdf::get_pdfs(dir)? {
+            let key = path
+                .file_stem()
+                .expect("file has no stem")
+                .to_str()
+                .unwrap();
+            if let Some(record) = records.get(&key.to_lowercase()) {
+                let mut metadata: HashMap<&str, &str> = HashMap::new();
+
+                let file_name = metadata::sanitize(&record.filename);
+                metadata.insert("file_name", &file_name);
+
+                let doc_type = format!(
+                    "{:?}",
+                    match record.second_level.as_ref() {
+                        "PIL" => model::DocType::Pil,
+                        "SPC" => model::DocType::Spc,
+                        _ => panic!("unexpected doc type"),
                     }
-                }
+                );
+                metadata.insert("doc_type", &doc_type);
+
+                let title = metadata::sanitize(&record.title);
+                metadata.insert("title", &title);
+
+                let rev_label = metadata::sanitize(&record.rev_label);
+                metadata.insert("rev_label", &rev_label);
+
+                let created = record.created.to_rfc3339();
+                metadata.insert("created", &created);
+
+                let release_state = metadata::sanitize(&record.release_state);
+                metadata.insert("release_state", &release_state);
+
+                let product_name = metadata::sanitize(&record.product_name);
+                metadata.insert("product_name", &product_name);
+
+                let substance_name = metadata::sanitize(&record.substance_name);
+                metadata.insert("substance_name", &substance_name);
+
+                storage::upload(&client, &mut core, &fs::read(path)?, &metadata)?;
             }
         }
     }
     Ok(())
-}
-
-fn get_doc_type(file_stem: &str) -> Option<model::DocType> {
-    if file_stem.starts_with("label-and-leaflet") {
-        Some(model::DocType::PilLabelAndLeaflet)
-    } else if file_stem.starts_with("label") {
-        Some(model::DocType::PilLabel)
-    } else if file_stem.starts_with("leaflet") {
-        Some(model::DocType::PilLeaflet)
-    } else if file_stem.starts_with("spc") {
-        Some(model::DocType::Spc)
-    } else {
-        None
-    }
 }
