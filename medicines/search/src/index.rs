e4cfaeee7d54dac7c77568d735e54b41081f7975
Matt Doughty <matt.doughty@mhra.gov.uk>
2019-11-29 15:53:45 +0000

    Changing index structure to support SPCs & PILs.

--- medicines/search/src/index.rs
+++ medicines/search/src/index.rs
@@ -2,12 +2,15 @@ use crate::azure_rest;
 use actix_web::client;
 
 pub fn create_index() -> Result<(), client::SendRequestError> {
+    let search_service = std::env::var("SEARCH_SERVICE")
+        .expect("Set env variable SEARCH_SERVICE first!");
     let index_definition = get_index_definition();
-    let url = "https://rb-mhra-mip.search.windows.net/indexes?api-version=2019-05-06";
+    let url = "https://SEARCH_SERVICE_PLACEHOLDER.search.windows.net/indexes?api-version=2019-05-06"
+        .replace("SEARCH_SERVICE_PLACEHOLDER", &search_service);
     let api_key = std::env::var("API_ADMIN_KEY")
         .expect("Set env variable API_ADMIN_KEY first!");
 
-    azure_rest::send_json_post_request(index_definition, url, &api_key)
+    azure_rest::send_json_post_request(index_definition, &url, &api_key)
 }
 
 fn get_index_definition() -> String {
