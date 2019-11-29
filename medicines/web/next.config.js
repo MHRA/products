78ce04a3fedfa40aa185c397de0184b0c8977bc4
Chris Caller <chris.caller@red-badger.com>
2019-11-29 16:52:20 +0000

    expose env vars to client

--- medicines/web/next.config.js
+++ medicines/web/next.config.js
@@ -1,15 +1,10 @@
-const Dotenv = require('dotenv-webpack');
-
+require('dotenv').config();
 module.exports = {
-  distDir: 'dist',
-  poweredByHeader: false,
-  webpack: config => {
-    config.plugins.push(new Dotenv());
-    return config;
+  env: {
+    AZURE_SEARCH_API_VERSION: process.env.AZURE_SEARCH_API_VERSION,
+    AZURE_SEARCH_INDEX: process.env.AZURE_SEARCH_INDEX,
+    AZURE_SEARCH_KEY: process.env.AZURE_SEARCH_KEY,
+    AZURE_SEARCH_SERVICE: process.env.AZURE_SEARCH_SERVICE,
+    AZURE_SEARCH_WORD_FUZZINESS: process.env.AZURE_SEARCH_WORD_FUZZINESS,
   },
-  // exportPathMap: function() {
-  //   return {
-  //     '/': { page: '/' },
-  //   };
-  // },
 };
