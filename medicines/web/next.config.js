41e1c9b30e6e91ddce3b97358ff539c470e0b4b3
Pedro Martin <pedro.martin@red-badger.com>
2019-12-02 11:28:42 +0000

    Set asset prefix as env variable

--- medicines/web/next.config.js
+++ medicines/web/next.config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 module.exports = {
   env: {
     AZURE_SEARCH_API_VERSION: process.env.AZURE_SEARCH_API_VERSION,
@@ -6,5 +7,9 @@ module.exports = {
     AZURE_SEARCH_KEY: process.env.AZURE_SEARCH_KEY,
     AZURE_SEARCH_SERVICE: process.env.AZURE_SEARCH_SERVICE,
     AZURE_SEARCH_WORD_FUZZINESS: process.env.AZURE_SEARCH_WORD_FUZZINESS,
+    AZURE_SEARCH_SCORING_PROFILE: process.env.AZURE_SEARCH_SCORING_PROFILE,
+    AZURE_SEARCH_EXACTNESS_BOOST: process.env.AZURE_SEARCH_EXACTNESS_BOOST,
   },
+  assetPrefix:
+    process.env.ASSET_PREFIX === 'master' ? '' : `/${process.env.ASSET_PREFIX}`,
 };
