34b0900e3c2cafe793eefc6a6a8437c51731ebe5
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2019-12-05 14:29:50 +0000

    Refactor getPaginationGroup

--- medicines/web/components/search-results/pagination.tsx
+++ medicines/web/components/search-results/pagination.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
 
+const getPaginationGroups = (pageCount: number, currentPage: number) => {
+  let firstGroup: number[] = [1];
+  let middleGroup: number[] = [];
+  let lastGroup: number[] = [pageCount];
+
+  switch (currentPage) {
+    case 1:
+    case 2:
+    case 3:
+      firstGroup = Array(currentPage + 1)
+        .fill(1)
+        .map((_, i) => i + 1);
+      break;
+    case pageCount - 2:
+    case pageCount - 1:
+    case pageCount:
+      lastGroup = Array(pageCount - currentPage + 2)
+        .fill(1)
+        .map((_, i) => currentPage - 1 + i);
+      break;
+    default:
+      middleGroup = [currentPage - 1, currentPage, currentPage + 1];
+      break;
+  }
+  return {
+    firstGroup,
+    middleGroup,
+    lastGroup,
+  };
+};
+
 const Pagination = (props: {
   searchTerm: string;
   resultCount: number;
@@ -10,29 +41,10 @@ const Pagination = (props: {
     `/?search=${props.searchTerm}&page=${p}`;
 
   const pageCount = Math.floor(props.resultCount / props.pageSize) + 1;
-  let firstGroup: number[] = [1];
-  let middleGroup: number[] = [];
-  let lastGroup: number[] = [pageCount];
-
-  if (props.currentPage === 1) {
-    firstGroup = [1, 2];
-  } else if (props.currentPage === 2) {
-    firstGroup = [1, 2, 3];
-  } else if (props.currentPage === 3) {
-    firstGroup = [1, 2, 3, 4];
-  } else if (props.currentPage === pageCount - 2) {
-    lastGroup = [pageCount - 3, pageCount - 2, pageCount - 1, pageCount];
-  } else if (props.currentPage === pageCount - 1) {
-    lastGroup = [pageCount - 2, pageCount - 1, pageCount];
-  } else if (props.currentPage === pageCount) {
-    lastGroup = [pageCount - 1, pageCount];
-  } else {
-    middleGroup = [
-      props.currentPage - 1,
-      props.currentPage,
-      props.currentPage + 1,
-    ];
-  }
+  const { firstGroup, middleGroup, lastGroup } = getPaginationGroups(
+    pageCount,
+    props.currentPage,
+  );
 
   const mapper = (p: number) => {
     if (p === props.currentPage) {
