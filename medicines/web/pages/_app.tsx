43814246831b991de515667351bc0ffac346a6e6
Matt Doughty <matt.doughty@mhra.gov.uk>
2019-12-06 12:14:00 +0000

    Enable debug in the dev environment.

--- medicines/web/pages/_app.tsx
+++ medicines/web/pages/_app.tsx
@@ -2,7 +2,7 @@ import App from 'next/app';
 import Router from 'next/router';
 import ReactGA from 'react-ga-gtm';
 
-ReactGA.initialize(process.env.GOOGLE_TRACKING_ID as string);
+ReactGA.initialize(process.env.GOOGLE_TRACKING_ID as string, { debug: true });
 Router.events.on('routeChangeComplete', url => ReactGA.pageview(url));
 
 export default App;
