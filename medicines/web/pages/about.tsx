c5c1f5e06eb07af35ddcd9e6d5e9e75d7b9a0f37
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2019-12-09 12:05:45 +0000

    Lorem ipsum About page and footer link

---
+++ medicines/web/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from '../components/page';
+
+const App: React.FC = () => {
+  return (
+    <Page title="About">
+      <p>Lorem ipsum dolore sit amet.</p>
+    </Page>
+  );
+};
+
+export default App;
