f844f289c07b87f22a35ed0ec64402400273b881
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2019-12-10 14:16:27 +0000

    Removing unused 'useEffect' import

--- medicines/web/pages/about.tsx
+++ medicines/web/pages/about.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import Page from '../components/page';
 import { mhra70 } from '../styles/colors';
