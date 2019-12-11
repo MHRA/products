be8ee089d551508d633a6e83ab49be54822622b9
Chris Caller <chris.caller@red-badger.com>
2019-12-11 14:34:15 +0000

    add GTM plugin

--- learning/web/gatsby-config.js
+++ learning/web/gatsby-config.js
@@ -36,6 +36,12 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        //id: `YOUR_GOOGLE_TAGMANAGER_ID`,
+      },
+    },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
