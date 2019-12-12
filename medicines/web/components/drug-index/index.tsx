13b8e2d64ed38b5045e14c8201ee6b13769e69ff
Stuart Harris <stuart.harris@red-badger.com>
2019-12-12 12:38:47 +0000

    wrap drug index on mobile

--- medicines/web/components/drug-index/index.tsx
+++ medicines/web/components/drug-index/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 import { IProduct, isIndex, isSubstance } from '../../model/substance';
+import { mobileBreakpoint } from '../../styles/dimensions';
 
 const StyledDrugIndex = styled.nav`
   h2 {
@@ -45,6 +46,15 @@ const StyledDrugIndex = styled.nav`
     text-decoration: underline;
     font-size: 1.1875rem;
     font-weight: normal;
+  }
+
+  @media ${mobileBreakpoint} {
+    ul.horizontal {
+      flex-wrap: wrap;
+    }
+    ul.horizontal > li {
+      flex-basis: 15%;
+    }
   }
 `;
 
