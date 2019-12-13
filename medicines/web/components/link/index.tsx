25c18a72e1375d21108724ded11e5f83976c8807
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2019-12-13 13:38:25 +0000

    Deduplicating slashes to fix homepage link issue

--- medicines/web/components/link/index.tsx
+++ medicines/web/components/link/index.tsx
@@ -2,16 +2,22 @@ import getConfig from 'next/config';
 import Link, { LinkProps } from 'next/link';
 import { UrlObject } from 'url';
 
+const deduplicateSlashes = (url: string) => {
+  return url.replace(/\/+/g, '/');
+};
+
 type Url = string | UrlObject;
 const MhraLink: React.FC<LinkProps> = props => {
   const { baseUrl, appendix } = getConfig().publicRuntimeConfig;
   let href: Url;
   if (typeof props.href === 'string') {
-    href = `${baseUrl}${props.href}${appendix}`;
+    href = deduplicateSlashes(`${baseUrl}${props.href}${appendix}`);
   } else {
     href = {
       ...props.href,
-      pathname: `${baseUrl}${props.href.pathname}${appendix}`,
+      pathname: deduplicateSlashes(
+        `${baseUrl}${props.href.pathname}${appendix}`,
+      ),
     };
   }
 
