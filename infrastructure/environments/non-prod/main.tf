c5658991b80d0d0de21a3e7d92db5ac0230d23c7
Pedro Martin <pedro.martin@red-badger.com>
2020-01-09 11:31:37 +0000

    AKS cluster created

--- infrastructure/environments/non-prod/main.tf
+++ infrastructure/environments/non-prod/main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
-  version         = "~> 1.38.0"
-  subscription_id = "b2f4966f-71ea-415b-9ec6-bf96d597d596"
+  version = "~> 1.38.0"
+  # subscription_id = "b2f4966f-71ea-415b-9ec6-bf96d597d596"
 }
 
 terraform {
@@ -90,4 +90,50 @@ resource "azurerm_storage_container" "cpd_website" {
 module "cpd_staticweb" {
   source               = "git@github.com:StefanSchoof/terraform-azurerm-static-website.git"
   storage_account_name = azurerm_storage_account.cpd.name
+}
+
+# AKS
+
+resource "azurerm_virtual_network" "network" {
+  name                = "aks-vnet"
+  location            = azurerm_resource_group.products.location
+  resource_group_name = azurerm_resource_group.products.name
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.products.name
+  address_prefix       = "10.1.0.0/24"
+  virtual_network_name = azurerm_virtual_network.network.name
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "aks"
+  location            = azurerm_resource_group.products.location
+  dns_prefix          = "aks"
+  resource_group_name = azurerm_resource_group.products.name
+
+  default_node_pool {
+    name       = "products"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+
+
+
+  tags = {
+    Environment = "non-prod"
+  }
+
+}
+
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.cluster.kube_config_raw
 }
--- infrastructure/environments/non-prod/variables.tf
+++ infrastructure/environments/non-prod/variables.tf
@@ -1,14 +1,14 @@
-variable "STORAGE_ACCOUNT_NAME" {
-  description = "Name of the Azure Storage Account used for Terraform state"
-}
+# variable "STORAGE_ACCOUNT_NAME" {
+#   description = "Name of the Azure Storage Account used for Terraform state"
+# }
 
-variable "CONTAINER_NAME" {
-  description = "Name of the Azure Blob Storage Container used for Terraform state"
-}
+# variable "CONTAINER_NAME" {
+#   description = "Name of the Azure Blob Storage Container used for Terraform state"
+# }
 
-variable "ACCESS_KEY" {
-  description = "Access Key for the Azure Storage Account used for Terraform state"
-}
+# variable "ACCESS_KEY" {
+#   description = "Access Key for the Azure Storage Account used for Terraform state"
+# }
 
 variable "REGION" {
   description = "Azure region"
