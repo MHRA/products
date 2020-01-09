8c994088d8b914d8b250da1eeaf7e8c247247649
Pedro Martin <pedro.martin@red-badger.com>
2020-01-09 14:28:09 +0000

    Script to generate a kubernetes config file

---
+++ infrastructure/scripts/create-kubernetes-config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+PS3='Please enter your current enviroment: '
+OPTIONS=("non-prod" "prod" "quit")
+K8_CONFIG_FILE="mhra-azure-kube-config.yml"
+
+select opt in "${OPTIONS[@]}"; do
+  case "$opt,$REPLY" in
+  non-prod,* | *,non-prod)
+    K8_CONFIG_FILE="non-prod-${K8_CONFIG_FILE}"
+    break
+    ;;
+  prod,* | *,prod)
+    K8_CONFIG_FILE="prod-${K8_CONFIG_FILE}"
+    break
+    ;;
+  quit,* | *,quit)
+    break
+    ;;
+  *) echo "invalid option $REPLY" ;;
+  esac
+done
+
+echo $K8_CONFIG_FILE
+
+echo "$(terraform output kube_config)" >~/$K8_CONFIG_FILE
+
+export KUBECONFIG=~/$K8_CONFIG_FILE
+
+echo "MHRA kubernetes config file was created, in ~/${K8_CONFIG_FILE}."
