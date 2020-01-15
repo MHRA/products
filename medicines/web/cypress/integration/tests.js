da98a56965bc40022c5ae38fb8109cfbd49f7d97
Craig Anderson <craiga@craiga.id.au>
2020-01-15 16:28:15 +0000

    First pass at Cypress tests.

---
+++ medicines/web/cypress/integration/tests.js
@@ -0,0 +1,31 @@
+describe('Search', function() {
+  it('Search for Paracetamol', function() {
+    cy.visit('/', {
+      onBeforeLoad: (win) => {
+        win.sessionStorage.clear()
+      },
+    })
+    cy.get("input[type='search']").type("paracetamol")
+    cy.contains("Search").click()
+    cy.contains("I have read and understand the disclaimer").click()
+    cy.contains("Agree").click()
+    cy.contains("Next").click()
+    cy.get("a[href*='docs/']")
+  })
+})
+describe('A-Z Index', function() {
+  it('Navigate to Paracetamol via A-Z index', function() {
+    cy.visit('/', {
+      onBeforeLoad: (win) => {
+        win.sessionStorage.clear()
+      },
+    })
+    cy.get("nav").contains("P").click()
+    cy.contains("PARACETAMOL").click()
+    cy.contains("PARACETAMOL TABLETS").click()
+    cy.contains("I have read and understand the disclaimer").click()
+    cy.contains("Agree").click()
+    cy.contains("Next").click()
+    cy.get("a[href*='docs/']")
+  })
+})
