c838eff64154830e318107aa9461b6b6ea8121bf
Craig Anderson <craiga@craiga.id.au>
2020-01-16 16:57:05 +0000

    Try loading Azure search results directly via Cypress

--- medicines/web/cypress/integration/tests.js
+++ medicines/web/cypress/integration/tests.js
@@ -1,4 +1,7 @@
 describe('Search', function() {
+  it('Can at least get to Azure search thingy', function() {
+    cy.request("https://mhraproductsdev.search.windows.net/indexes/products-index/docs?api-key=CFBCBE8AA11AA871C14001527533870C&api-version=2017-11-11&highlight=content&queryType=full&%24count=true&%24top=10&%24skip=0&search=paracetamol%7E1+paracetamol%5E4&scoringProfile=preferKeywords")
+  }),
   it('Search for Paracetamol', function() {
     cy.visit('/', {
       onBeforeLoad: win => {
