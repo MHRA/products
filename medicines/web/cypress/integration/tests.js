19757204474255e39dcf8bae1a5451069b542e49
Craig Anderson <craiga@craiga.id.au>
2020-01-21 09:29:09 +0000

    Remove duplicated and document code.

--- medicines/web/cypress/integration/tests.js
+++ medicines/web/cypress/integration/tests.js
@@ -1,76 +1,75 @@
+// We need to replace window.fetch with an XHR-based implementation so that Cypress
+// can mock out the endpoints we use.
+// See https://docs.cypress.io/guides/guides/network-requests.html#Testing-Strategies
+// and https://github.com/cypress-io/cypress/issues/95.
+
+// Some code to do this is adapted from https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/stubbing-spying__window-fetch/cypress/integration/polyfill-fetch-from-tests-spec.js
+
+let polyfill;
+
+// grab fetch polyfill from remote URL, could be also from a local package
+before(() => {
+  const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
+
+  cy.request(polyfillUrl).then(response => {
+    polyfill = response.body;
+  });
+});
+
+Cypress.on('window:before:load', (win) => {
+  delete win.fetch;
+  // since the application code does not ship with a polyfill
+  // load a polyfilled "fetch" from the test
+  win.eval(polyfill);
+  win.fetch = win.unfetch;
+
+  // Clear out session storage so that the disclaimer is always presented.
+  win.sessionStorage.clear();
+})
+
+
 describe('Search', function() {
-  let polyfill
-
-  // grab fetch polyfill from remote URL, could be also from a local package
-  before(() => {
-    const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js'
-
-    cy.request(polyfillUrl)
-    .then((response) => {
-      polyfill = response.body
-    })
-  })
-
   it('Search for Paracetamol', function() {
+    // Mock out endpoints.
     cy.server();
     cy.route(
       'https://mhraproductsdev.search.windows.net/indexes/products-index/docs?api-key=CFBCBE8AA11AA871C14001527533870C&api-version=2017-11-11&highlight=content&queryType=full&$count=true&$top=10&$skip=0&search=paracetamol~1+paracetamol^4&scoringProfile=preferKeywords',
-      "fixture:search_results.json");
+      'fixture:search_results.json',
+    );
     cy.route(
       'https://mhraproductsdev.search.windows.net/indexes/products-index/docs?api-key=CFBCBE8AA11AA871C14001527533870C&api-version=2017-11-11&highlight=content&queryType=full&$count=true&$top=10&$skip=10&search=paracetamol~1+paracetamol^4&scoringProfile=preferKeywords',
-      "fixture:search_results.json");
-    cy.visit('/', {
-      onBeforeLoad: win => {
-        delete win.fetch;
-        // since the application code does not ship with a polyfill
-        // load a polyfilled "fetch" from the test
-        win.eval(polyfill);
-        win.fetch = win.unfetch;
-        win.sessionStorage.clear();
-      },
-    });
-    cy.get('input[type=\'search\']').type('paracetamol');
+      'fixture:search_results.json',
+    );
+
+    cy.visit('/');
+    cy.get("input[type='search']").type('paracetamol');
     cy.contains('Search').click();
     cy.contains('I have read and understand the disclaimer').click();
     cy.contains('Agree').click();
     cy.contains('Next').click();
-    cy.get('a[href=\'https://example.com/my-cool-document.pdf\']');
+    cy.get("a[href='https://example.com/my-cool-document.pdf']");
   });
 });
+
+
 describe('A-Z Index', function() {
-  let polyfill
-
-  // grab fetch polyfill from remote URL, could be also from a local package
-  before(() => {
-    const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js'
-
-    cy.request(polyfillUrl)
-    .then((response) => {
-      polyfill = response.body
-    })
-  })
   it('Navigate to Paracetamol via A-Z index', function() {
+    // Mock out endpoints.
     cy.server();
     cy.route(
       "https://mhraproductsdev.search.windows.net/indexes/products-index/docs?api-key=CFBCBE8AA11AA871C14001527533870C&api-version=2017-11-11&facet=facets,count:50000,sort:value&$filter=facets/any(f:+f+eq+'P')&$top=0",
-      "fixture:facets.json");
+      'fixture:facets.json',
+    );
     cy.route(
-      "https://mhraproductsdev.search.windows.net/indexes/products-index/docs?api-key=CFBCBE8AA11AA871C14001527533870C&api-version=2017-11-11&highlight=content&queryType=full&$count=true&$top=10&$skip=0&search=PARACETAMOL~1+PARACETAMOL^4+TABLETS~1+TABLETS^4&scoringProfile=preferKeywords",
-      "fixture:search_results.json");
+      'https://mhraproductsdev.search.windows.net/indexes/products-index/docs?api-key=CFBCBE8AA11AA871C14001527533870C&api-version=2017-11-11&highlight=content&queryType=full&$count=true&$top=10&$skip=0&search=PARACETAMOL~1+PARACETAMOL^4+TABLETS~1+TABLETS^4&scoringProfile=preferKeywords',
+      'fixture:search_results.json',
+    );
     cy.route(
-      "https://mhraproductsdev.search.windows.net/indexes/products-index/docs?api-key=CFBCBE8AA11AA871C14001527533870C&api-version=2017-11-11&highlight=content&queryType=full&$count=true&$top=10&$skip=10&search=PARACETAMOL~1+PARACETAMOL^4+TABLETS~1+TABLETS^4&scoringProfile=preferKeywords",
-      "fixture:search_results.json");
-    
-    cy.visit('/', {
-      onBeforeLoad: win => {
-        delete win.fetch;
-        // since the application code does not ship with a polyfill
-        // load a polyfilled "fetch" from the test
-        win.eval(polyfill);
-        win.fetch = win.unfetch;
-        win.sessionStorage.clear();
-      },
-    });
+      'https://mhraproductsdev.search.windows.net/indexes/products-index/docs?api-key=CFBCBE8AA11AA871C14001527533870C&api-version=2017-11-11&highlight=content&queryType=full&$count=true&$top=10&$skip=10&search=PARACETAMOL~1+PARACETAMOL^4+TABLETS~1+TABLETS^4&scoringProfile=preferKeywords',
+      'fixture:search_results.json',
+    );
+
+    cy.visit('/');
     cy.get('nav')
       .contains('P')
       .click();
@@ -79,6 +78,6 @@ describe('A-Z Index', function() {
     cy.contains('I have read and understand the disclaimer').click();
     cy.contains('Agree').click();
     cy.contains('Next').click();
-    cy.get('a[href=\'https://example.com/my-cool-document.pdf\']');
+    cy.get("a[href='https://example.com/my-cool-document.pdf']");
   });
 });
