4687bec7b01679bcfc14ddca6be8a963dbd68dcf
Pedro Martin <pedro.martin@red-badger.com>
2020-01-21 14:19:47 +0000

    Abstract CPD resources as module

--- infrastructure/modules/products/outputs.tf
+++ infrastructure/modules/products/outputs.tf
@@ -5,3 +5,7 @@ output "products-primary-access-key" {
 output "products-static-web-url" {
   value = azurerm_storage_account.products.primary_web_endpoint
 }
+
+output "search_admin_key" {
+  value = azurerm_search_service.search.primary_key
+}
