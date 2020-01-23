46c8ff44f7dcbc528950255905f08d7be0d0f758
Pedro Martin <pedro.martin@red-badger.com>
2020-01-23 15:57:38 +0000

    Create a ingress

--- infrastructure/environments/development/outputs.tf
+++ infrastructure/environments/development/outputs.tf
@@ -7,3 +7,10 @@ output "container_registry_id" {
 output "container_registry_login_server" {
   value = module.cluster.container_registry_login_server
 }
+
+output "public_ip" {
+  value = module.cluster.public_ip
+}
+output "host" {
+  value = module.cluster.host
+}
--- infrastructure/modules/cluster/outputs.tf
+++ infrastructure/modules/cluster/outputs.tf
@@ -13,3 +13,13 @@ output "container_registry_id" {
 output "container_registry_login_server" {
   value = azurerm_container_registry.container_registry.login_server
 }
+
+
+output "public_ip" {
+  value = azurerm_public_ip.products_ip.ip_address
+}
+
+
+output "host" {
+  value = azurerm_kubernetes_cluster.cluster.kube_config.0.host
+}
