ed6934eeccdf06fc47cac72c4cdc105318c419c8
Pedro Martin <pedro.martin@red-badger.com>
2020-01-28 15:53:32 +0000

    Add quotes for consistency

--- infrastructure/modules/cluster/outputs.tf
+++ infrastructure/modules/cluster/outputs.tf
@@ -14,6 +14,6 @@ output "host" {
   value = azurerm_kubernetes_cluster.cluster.kube_config.0.host
 }
 
-output resource_group_name {
+output "resource_group_name" {
   value = azurerm_kubernetes_cluster.cluster.resource_group_name
 }
