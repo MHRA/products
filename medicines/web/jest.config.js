c12982a0b030791ec8c02f3aabb3be05fba43f47
Pedro Martin <pedro.martin@red-badger.com>
2020-01-30 14:51:26 +0000

    Remove deprecation warnings

--- medicines/web/jest.config.js
+++ medicines/web/jest.config.js
@@ -6,12 +6,12 @@ module.exports = {
   testMatch: ['**/*.(test|spec).(ts|tsx)'],
   globals: {
     'ts-jest': {
-      useBabelrc: true,
-      tsConfigFile: 'jest.tsconfig.json',
+      babelConfig: true,
+      tsConfig: 'jest.tsconfig.json',
     },
   },
-  coveragePathIgnorePatterns: ['/node_modules/', 'enzyme.js'],
-  setupTestFrameworkScriptFile: '<rootDir>/enzyme.js',
+  coveragePathIgnorePatterns: ['/node_modules/', 'enzyme.ts'],
+  setupFilesAfterEnv: ['<rootDir>/enzyme.ts'],
   coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
