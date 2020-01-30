61aef7027fa55ed45a404c6d014b777bd1bf8a71
Pedro Martin <pedro.martin@red-badger.com>
2020-01-30 15:39:39 +0000

    Test footer component

--- medicines/web/src/components/disclaimer/index.test.tsx
+++ medicines/web/src/components/disclaimer/index.test.tsx
@@ -2,12 +2,10 @@ import { shallow } from 'enzyme';
 import React from 'react';
 import Disclaimer from './index';
 
-const fn = jest.fn();
-
 describe(Disclaimer, () => {
-  it('should render ⚠️', () => {
+  it('should render', () => {
     const component = shallow(
-      <Disclaimer onDisclaimerAgree={fn} searchTerm={'Perinola'} />,
+      <Disclaimer onDisclaimerAgree={jest.fn()} searchTerm={'Perinola'} />,
     );
     expect(component).toMatchSnapshot();
   });
