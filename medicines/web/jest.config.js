32296b831ac95d8649dc8771a054817e1e0573a0
Pedro Martin <pedro.martin@red-badger.com>
2020-02-03 10:03:15 +0000

    Serialise snaphots

--- medicines/web/jest.config.js
+++ medicines/web/jest.config.js
@@ -1,21 +1,22 @@
 module.exports = {
+  coveragePathIgnorePatterns: ['/node_modules/', 'enzyme.ts'],
+  coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'md'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testMatch: ['**/*.(test|spec).(ts|tsx)'],
   globals: {
     'ts-jest': {
       babelConfig: '<rootDir>/.babelrc',
       tsConfig: '<rootDir>/jest.tsconfig.json',
     },
   },
-  coveragePathIgnorePatterns: ['/node_modules/', 'enzyme.ts'],
-  setupFilesAfterEnv: ['<rootDir>/config/enzyme.ts'],
-  coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
       '<rootDir>/config/assets-transformer.ts',
     '\\.(css|less|scss)$': '<rootDir>/config/assets-transformer.ts',
+  },
+  setupFilesAfterEnv: ['<rootDir>/config/enzyme.ts'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testMatch: ['**/*.(test|spec).(ts|tsx)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
   },
 };
