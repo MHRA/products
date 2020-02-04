0238c89dd444721ae3a7d37553560a74c1ebd8d6
Pedro Martin <pedro.martin@red-badger.com>
2020-02-04 12:34:34 +0000

    Add a new route to check deployment

--- medicines/api/src/main.rs
+++ medicines/api/src/main.rs
@@ -38,6 +38,10 @@ async fn healthz() -> impl actix_web::Responder {
     "OK"
 }
 
+async fn deploy_test() -> impl actix_web::Responder {
+    "I deployed this using AZ pipeline 💥"
+}
+
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info, actix_server=info");
@@ -58,6 +62,7 @@ async fn main() -> io::Result<()> {
             .service(web::resource("/graphql").route(web::post().to(graphql)))
             .service(web::resource("/graphiql").route(web::get().to(graphiql)))
             .service(web::resource("/healthz").route(web::get().to(healthz)))
+            .service(web::resource("/deploy-test").route(web::get().to(deploy_test)))
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0)? {
