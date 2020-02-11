3bc2498751e44145e10c347b6e0a09adda03ff50
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-02-11 11:45:00 +0000

    Refactoring DocTypeCheckbox to reduce repeated html

--- medicines/web/src/components/search-filter/index.tsx
+++ medicines/web/src/components/search-filter/index.tsx
@@ -13,51 +13,42 @@ interface ISearchFilterProps {
   checkDocType: (d: DocType) => void;
 }
 
+interface IDocTypeCheckboxProps extends ISearchFilterProps {
+  docType: DocType;
+  name: string;
+}
+
+const DocTypeCheckbox: React.FC<IDocTypeCheckboxProps> = props => {
+  const { docType: d, name, checkDocType, docTypes } = props;
+  const handleCheck = () => checkDocType(d);
+  const id = `filter-${d.toLowerCase()}`;
+  return (
+    <p>
+      <input
+        type="checkbox"
+        id={id}
+        name="doc"
+        value={d}
+        checked={docTypes.includes(d)}
+        onChange={handleCheck}
+      />
+      <label htmlFor={id}>
+        {name} ({d.toUpperCase()})
+      </label>
+    </p>
+  );
+};
+
 const SearchFilter: React.FC<ISearchFilterProps> = props => {
-  const { docTypes, checkDocType } = props;
-
-  const checkDocTypeGetter = (d: DocType) => {
-    return () => checkDocType(d);
+  const generateCheckbox = (d: DocType, n: string) => {
+    return <DocTypeCheckbox {...props} docType={d} name={n} />;
   };
-
   return (
     <StyledSearchFilter>
       <h2>Filter documents by</h2>
-      <p>
-        <input
-          type="checkbox"
-          id="filter-spc"
-          name="doc"
-          value="Spc"
-          checked={docTypes.includes(DocType.Spc)}
-          onChange={checkDocTypeGetter(DocType.Spc)}
-        />
-        <label htmlFor="filter-spc">
-          Summary of Product Characteristics (SPC)
-        </label>
-      </p>
-      <p>
-        <input
-          type="checkbox"
-          id="filter-pil"
-          name="doc"
-          value="Pil"
-          checked={docTypes.includes(DocType.Pil)}
-          onChange={checkDocTypeGetter(DocType.Pil)}
-        />
-        <label htmlFor="filter-pil">Patient Information Leaflet (PIL)</label>
-      </p>
-      <p>
-        <input
-          type="checkbox"
-          id="filter-par"
-          name="doc"
-          value="Par"
-          checked={docTypes.includes(DocType.Par)}
-          onChange={checkDocTypeGetter(DocType.Par)}
-        />
-        <label htmlFor="filter-par">Public Assesment Reports (PAR)</label>
-      </p>
+      {generateCheckbox(DocType.Spc, 'Summary of Product Characteristics')}
+      {generateCheckbox(DocType.Pil, 'Patient Information Leaflet')}
+      {generateCheckbox(DocType.Par, 'Public Assesment Reports')}
     </StyledSearchFilter>
   );
 };
