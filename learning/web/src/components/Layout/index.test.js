ed6668128d4a36335e12cf61c1f6ea2f2be1a095
Pedro Martin <pedro.martin@red-badger.com>
2020-02-17 15:30:50 +0000

    Write shallow test for Layout

---
+++ learning/web/src/components/Layout/index.test.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Layout from "./index"
+import { shallow } from "enzyme"
+
+const location = { pathname: "test" }
+
+describe(Layout, () => {
+  it("should render 🍪", () => {
+    const component = shallow(
+      <Layout
+        location={location}
+        title={"Hola"}
+        children={<></>}
+        withSidebar={true}
+      />
+    )
+    expect(component).toMatchSnapshot()
+  })
+})
