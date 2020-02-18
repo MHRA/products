ffdc229e7b49dc8552f641ad5a65b5ecde8886b7
Stuart Harris <stuart.harris@red-badger.com>
2020-02-18 17:20:49 +0000

    Return json status

--- medicines/doc-index-updater/src/state_manager/mod.rs
+++ medicines/doc-index-updater/src/state_manager/mod.rs
@@ -1,9 +1,28 @@
+use serde_derive::Serialize;
 use uuid::Uuid;
 use warp::Filter;
 
+#[derive(Serialize)]
+enum JobStatus {
+    Accepted,
+    _Done,
+    _NotFound,
+}
+
+#[derive(Serialize)]
+struct JobStatusResponse {
+    id: Uuid,
+    status: JobStatus,
+}
+
 #[tokio::main]
 pub async fn serve() {
-    let hello = warp::path!("jobs" / Uuid).map(|name| format!("Hello, {}!", name));
+    let job_status = warp::path!("jobs" / Uuid).map(|id: Uuid| {
+        warp::reply::json(&JobStatusResponse {
+            id,
+            status: JobStatus::Accepted,
+        })
+    });
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(job_status).run(([127, 0, 0, 1], 3030)).await;
 }
