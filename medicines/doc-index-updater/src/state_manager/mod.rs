d389d541e89e9869736603e9c54a9e0d17a72eda
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-02-19 09:29:37 +0000

    Extracting handler for state_manager to method

--- medicines/doc-index-updater/src/state_manager/mod.rs
+++ medicines/doc-index-updater/src/state_manager/mod.rs
@@ -15,14 +15,17 @@ struct JobStatusResponse {
     status: JobStatus,
 }
 
+fn handler(id: Uuid) -> impl warp::reply::Reply {
+    warp::reply::json(&JobStatusResponse {
+        id,
+        status: JobStatus::Accepted,
+    })
+}
+
 #[tokio::main]
 pub async fn serve() {
-    let job_status = warp::path!("jobs" / Uuid).map(|id: Uuid| {
-        warp::reply::json(&JobStatusResponse {
-            id,
-            status: JobStatus::Accepted,
-        })
-    });
-    let routes = job_status.with(warp::log("doc_index_updater"));
+    let routes = warp::path!("jobs" / Uuid)
+        .map(handler)
+        .with(warp::log("doc_index_updater"));
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
