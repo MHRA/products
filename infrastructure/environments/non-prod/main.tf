f66becb444106273e3d0073fd8d6bce7e756fa44
Pedro Martin <pedro.martin@red-badger.com>
2020-02-19 14:52:14 +0000

    Fix resource group and namespaces

--- infrastructure/environments/non-prod/main.tf
+++ infrastructure/environments/non-prod/main.tf
@@ -12,37 +12,44 @@ terraform {
 }
 
 locals {
-  resource_group_name = var.RESOURCE_GROUP_PRODUCTS
-  location            = var.REGION
-  environment         = var.ENVIRONMENT
   client_id           = var.CLIENT_ID
   client_secret       = var.CLIENT_SECRET
+  environment         = var.ENVIRONMENT
+  location            = var.REGION
+  products_namespace  = "mhraproductsdev"
+  cpd_name_space      = "mhracpddev"
+  resource_group_name = var.RESOURCE_GROUP_PRODUCTS
 }
 
 # website
 module "products" {
-  source              = "../../modules/products"
-  resource_group_name = local.resource_group_name
-  location            = local.location
+  source = "../../modules/products"
+
   environment         = local.environment
+  location            = local.location
+  namespace           = local.products_namespace
+  resource_group_name = local.resource_group_name
 }
 
 # AKS
 module cluster {
-  source              = "../../modules/cluster"
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  environment         = local.environment
+  source = "../../modules/cluster"
+
   client_id           = local.client_id
   client_secret       = local.client_secret
+  environment         = local.environment
+  location            = local.location
+  resource_group_name = local.resource_group_name
 }
 
 # CPD
 module cpd {
-  source              = "../../modules/cpd"
+  source = "../../modules/cpd"
+
   resource_group_name = local.resource_group_name
   location            = local.location
   environment         = local.environment
+  namespace           = local.cpd_name_space
 }
 
 
