eb5b1ccccb212385df26fb96669c6e37002e2ba3
Pedro Martin <pedro.martin@red-badger.com>
2020-02-20 11:04:44 +0000

    Set more granular outputs

--- infrastructure/modules/cluster/outputs.tf
+++ infrastructure/modules/cluster/outputs.tf
@@ -1,19 +1,19 @@
-output "client_certificate" {
+output "api_cluster_client_certificate" {
   value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
 }
 
-output "kube_config" {
+output "api_cluster_kube_config" {
   value = azurerm_kubernetes_cluster.cluster.kube_config_raw
 }
 
-output "public_ip" {
+output "api_cluster_public_ip" {
   value = azurerm_public_ip.products_ip.ip_address
 }
 
-output "host" {
+output "api_cluster_host" {
   value = azurerm_kubernetes_cluster.cluster.kube_config.0.host
 }
 
-output "resource_group_name" {
+output "api_cluster_resource_group_name" {
   value = azurerm_kubernetes_cluster.cluster.resource_group_name
 }
