af60f88dffb9b04f9c55bb096bf4e8855a3bfe54
Craig Anderson <craiga@craiga.id.au>
2020-02-26 16:30:45 +0000

    Products API

--- medicines/api/src/azure_search.rs
+++ medicines/api/src/azure_search.rs
@@ -15,17 +15,17 @@ pub struct AzureResult {
     pub product_name: Option<String>,
     pub substance_name: Vec<String>,
     pub title: String,
-    created: String,
-    facets: Vec<String>,
-    keywords: Option<String>,
-    metadata_storage_size: i32,
-    release_state: String,
-    rev_label: Option<String>,
-    suggestions: Vec<String>,
+    pub created: String,
+    pub facets: Vec<String>,
+    pub keywords: Option<String>,
+    pub metadata_storage_size: i32,
+    pub release_state: String,
+    pub rev_label: Option<String>,
+    pub suggestions: Vec<String>,
     #[serde(rename = "@search.score")]
-    score: f32,
+    pub score: f32,
     #[serde(rename = "@search.highlights")]
-    highlights: AzureHighlight,
+    pub highlights: Option<AzureHighlight>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -96,6 +96,15 @@ impl AzureSearchClient {
     ) -> Result<AzureSearchResults, reqwest::Error> {
         azure_search(search_term, &self.client, &self.config).await
     }
+
+    pub async fn filter_by_collection(
+        &self,
+        field_name: String,
+        value: String,
+        operator: String,
+    ) -> Result<AzureSearchResults, reqwest::Error> {
+        filter_by_collection(field_name, value, operator, &self.client, &self.config).await
+    }
 }
 
 async fn azure_search(
@@ -131,4 +140,101 @@ async fn azure_search(
         .await?
         .json::<AzureSearchResults>()
         .await
+}
+
+fn build_filter_by_collection_request(
+    field_name: &String,
+    value: &String,
+    operator: &String,
+    client: &reqwest::Client,
+    config: &AzureConfig,
+) -> Result<reqwest::Request, reqwest::Error> {
+    let base_url = format!(
+        "https://{search_service}.search.windows.net/indexes/{search_index}/docs",
+        search_service = config.search_service,
+        search_index = config.search_index
+    );
+
+    let filter = format!(
+        "{field_name}/any(value: value {operator} '{value}')",
+        field_name = field_name,
+        value = value,
+        operator = operator,
+    );
+
+    client
+        .get(&base_url)
+        .query(&[
+            ("api-version", "2017-11-11"),
+            ("api-key", &config.api_key),
+            ("$filter", &filter),
+        ])
+        .build()
+}
+
+async fn filter_by_collection(
+    field_name: String,
+    value: String,
+    operator: String,
+    client: &reqwest::Client,
+    config: &AzureConfig,
+) -> Result<AzureSearchResults, reqwest::Error> {
+    let req = build_filter_by_collection_request(&field_name, &value, &operator, &client, &config)?;
+    client
+        .execute(req)
+        .await?
+        .json::<AzureSearchResults>()
+        .await
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_build_filter_by_collection_request() {
+        let client = reqwest::Client::new();
+        let config = AzureConfig {
+            search_service: "my_cool_service".to_string(),
+            search_index: "my_cool_search_index".to_string(),
+            api_key: "my_cool_api_key".to_string(),
+        };
+
+        let req = build_filter_by_collection_request(
+            &"my_cool_field".to_string(),
+            &"my cool value".to_string(),
+            &"cooler_than".to_string(),
+            &client,
+            &config,
+        )
+        .unwrap();
+
+        let url = req.url();
+        assert_eq!(url.scheme(), "https");
+        assert_eq!(url.host_str(), Some("my_cool_service.search.windows.net"));
+        assert_eq!(url.path(), "/indexes/my_cool_search_index/docs");
+
+        let mut query = url.query_pairs();
+        assert_eq!(
+            query
+                .find(|query_pair| query_pair.0 == "api-version")
+                .unwrap()
+                .1,
+            "2017-11-11"
+        );
+        assert_eq!(
+            query
+                .find(|query_pair| query_pair.0 == "api-key")
+                .unwrap()
+                .1,
+            "my_cool_api_key"
+        );
+        assert_eq!(
+            query
+                .find(|query_pair| query_pair.0 == "$filter")
+                .unwrap()
+                .1,
+            "my_cool_field/any(value: value cooler_than 'my cool value')"
+        );
+    }
 }
