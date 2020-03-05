dc4d013f8de2ef839f4dbd257039bb9c1d13762a
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-03-05 15:28:06 +0000

    Sending the message to the queue, then passing when I get it back

--- medicines/doc-index-updater/tests/delete_manager.rs
+++ medicines/doc-index-updater/tests/delete_manager.rs
@@ -1,13 +1,27 @@
 extern crate doc_index_updater;
 
+use core::{fmt::Debug, future::Future};
 use doc_index_updater::{delete_manager, service_bus_client::delete_factory};
 use tokio_test::block_on;
 
 #[test]
 #[ignore]
 fn delete_manager_works() {
+    let sent_message = "This is the test message";
     let mut delete_client = block_on(delete_factory()).unwrap();
-    let a = block_on(delete_manager::get_message(&mut delete_client)).unwrap();
+    get_ok(delete_client.send_event(sent_message, time::Duration::seconds(1)));
 
-    assert_eq!(a, "free");
+    let mut received_message = get_ok(delete_manager::get_message(&mut delete_client));
+    while received_message != sent_message {
+        received_message = get_ok(delete_manager::get_message(&mut delete_client));
+    }
+
+    assert_eq!(received_message, sent_message);
+}
+
+fn get_ok<T, U>(spawn: impl Future<Output = Result<T, U>>) -> T
+where
+    U: Debug,
+{
+    block_on(spawn).unwrap()
 }
