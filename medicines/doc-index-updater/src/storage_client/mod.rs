9f4c9b1d92c95984a46ca72ac945fada995cf87f
Tim Lee <tim.lee@red-badger.com>
2020-03-15 18:30:18 +0000

    Revert changes to storage client and delete manager

--- medicines/doc-index-updater/src/storage_client/mod.rs
+++ medicines/doc-index-updater/src/storage_client/mod.rs
@@ -1,7 +1,7 @@
-use anyhow::anyhow;
+use azure_sdk_core::errors::AzureError;
 use azure_sdk_storage_core::prelude::Client;
 
-pub fn factory() -> Result<Client, anyhow::Error> {
+pub fn factory() -> Result<Client, AzureError> {
     let storage_account =
         std::env::var("STORAGE_ACCOUNT").expect("Set env variable STORAGE_ACCOUNT first!");
 
@@ -9,8 +9,4 @@ pub fn factory() -> Result<Client, anyhow::Error> {
         std::env::var("STORAGE_MASTER_KEY").expect("Set env variable STORAGE_MASTER_KEY first!");
 
     Client::new(&storage_account, &master_key)
-        .map_err(|e| {
-            tracing::error!("{:?}", e);
-            anyhow!("Couldn't create storage client")
-        })
 }
