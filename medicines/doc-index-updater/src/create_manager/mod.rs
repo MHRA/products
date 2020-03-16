aaedfa3dac36b043e86d9733d46bb82a48a0f01c
Tim Lee <tim.lee@red-badger.com>
2020-03-16 15:54:38 +0000

    Format files using cargo fmt

--- medicines/doc-index-updater/src/create_manager/mod.rs
+++ medicines/doc-index-updater/src/create_manager/mod.rs
@@ -4,7 +4,7 @@ use crate::{
         create_factory, DocIndexUpdaterQueue, RetrieveFromQueueError, RetrievedMessage,
     },
     state_manager::StateManager,
-    storage_client
+    storage_client,
 };
 
 use anyhow::anyhow;
@@ -14,8 +14,8 @@ use std::{collections::HashMap, time::Duration};
 use tokio::time::delay_for;
 
 mod hash;
-mod sftp_client;
 mod metadata;
+mod sftp_client;
 
 #[tracing::instrument]
 pub async fn create_service_worker(
@@ -30,7 +30,7 @@ pub async fn create_service_worker(
     loop {
         match try_process_from_queue(&mut create_client, &state_manager).await {
             Ok(()) => {}
-            Err(e) => tracing::error!("{:?}", e)
+            Err(e) => tracing::error!("{:?}", e),
         }
         delay_for(time_to_wait).await;
     }
@@ -43,42 +43,43 @@ async fn try_process_from_queue(
     tracing::info!("Checking for create messages");
     let retrieved_result: Result<RetrievedMessage<CreateMessage>, RetrieveFromQueueError> =
         service_bus_client.receive().await;
-    
+
     if let Ok(retrieval) = retrieved_result {
         let processing_result = process_message(retrieval.message.clone()).await;
         match processing_result {
             Ok(FileProcessStatus::Success(job_id)) => {
                 let _ = state_manager.set_status(job_id, JobStatus::Done).await?;
-            },
-            Ok(FileProcessStatus::NothingToProcess) => {},
+            }
+            Ok(FileProcessStatus::NothingToProcess) => {}
             Err(e) => {
-                if e.to_string() == "Couldn't retrieve file: [-31] Failed opening remote file".to_string() {
+                if e.to_string()
+                    == "Couldn't retrieve file: [-31] Failed opening remote file".to_string()
+                {
                     tracing::info!("Updating state to errored and removing message");
                     let _ = state_manager.set_status(
-                        retrieval.message.job_id, 
+                        retrieval.message.job_id,
                         JobStatus::Error {
-                            message: "Couldn't find file".to_string(), 
-                            code:"404".to_string()
-                        });
+                            message: "Couldn't find file".to_string(),
+                            code: "404".to_string(),
+                        },
+                    );
                     // TODO: Uncomment before releasing
                     // or when we have a centralised SFTP server for dev
                     // let _ = retrieval.remove().await?;
                 }
                 return Err(e);
-            },
+            }
         };
     }
     Ok(())
 }
 
-async fn process_message(
-    message: CreateMessage
-) -> Result<FileProcessStatus, anyhow::Error> {
+async fn process_message(message: CreateMessage) -> Result<FileProcessStatus, anyhow::Error> {
     tracing::info!("Message received: {:?} ", message);
 
     let file = sftp_client::retrieve(
-        message.document.file_source.clone(), 
-        message.document.file_path.clone()
+        message.document.file_source.clone(),
+        message.document.file_path.clone(),
     )
     .await
     .map_err(|e| anyhow!("Couldn't retrieve file: {:?}", e))?;
@@ -87,10 +88,18 @@ async fn process_message(
     let file_digest = md5::compute(&file[..]);
     let file_data_hash = hash::sha1(&file);
     let _blob = create_blob(&file_data_hash, &file, &metadata, &file_digest).await?;
-    tracing::info!("Uploaded blob {} for job {}", &file_data_hash, &message.job_id);
+    tracing::info!(
+        "Uploaded blob {} for job {}",
+        &file_data_hash,
+        &message.job_id
+    );
 
     add_to_search_index("blob_data".to_string()).await;
-    tracing::info!("Added to index {} for job {}", &file_data_hash, &message.job_id);
+    tracing::info!(
+        "Added to index {} for job {}",
+        &file_data_hash,
+        &message.job_id
+    );
 
     Ok(FileProcessStatus::Success(message.job_id))
 }
@@ -99,14 +108,14 @@ pub async fn create_blob(
     blob_name: &str,
     file_data: &[u8],
     metadata: &HashMap<String, String>,
-    file_digest: &md5::Digest
+    file_digest: &md5::Digest,
 ) -> Result<(), anyhow::Error> {
     let storage_client = storage_client::factory()
         .map_err(|e| anyhow!("Couldn't create storage client: {:?}", e))?;
 
-    let container_name = std::env::var("STORAGE_CONTAINER")
-        .expect("Set env variable STORAGE_CONTAINER first!");
-    let mut metadata_ref : HashMap<&str, &str> = HashMap::new();
+    let container_name =
+        std::env::var("STORAGE_CONTAINER").expect("Set env variable STORAGE_CONTAINER first!");
+    let mut metadata_ref: HashMap<&str, &str> = HashMap::new();
     for (key, val) in metadata {
         metadata_ref.insert(&key, &val);
     }
