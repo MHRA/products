f026a66f0187f36470db482cf3fa2c9c1f5d3f91
Matt Doughty <matt.doughty@mhra.gov.uk>
2020-03-19 13:31:40 +0000

    Add testing for Basic Auth

--- medicines/doc-index-updater/src/auth_manager/mod.rs
+++ medicines/doc-index-updater/src/auth_manager/mod.rs
@@ -1,14 +1,10 @@
+extern crate base64;
+
 use regex::Regex;
 use warp::{Filter, Rejection};
 
-#[derive(Debug)]
-struct NoAuthenticationPassed;
-#[derive(Debug)]
-struct CorruptAuthenticationPassed;
-#[derive(Debug)]
-struct AuthenticationFailed;
-impl warp::reject::Reject for NoAuthenticationPassed {}
-impl warp::reject::Reject for CorruptAuthenticationPassed {}
+#[derive(Clone, Debug)]
+pub struct AuthenticationFailed;
 impl warp::reject::Reject for AuthenticationFailed {}
 
 fn get_basic_username() -> String {
@@ -23,17 +19,13 @@ fn auth_is_correct(username: String, password: String) -> bool {
     username == get_basic_username() && password == get_basic_password()
 }
 
-fn extract_auth_from_header(auth_header: Option<String>) -> Option<String> {
-    if let Some(auth_string) = auth_header {
-        let re = Regex::new(r"^Basic\s(?P<encoded_credentials>[-A-Za-z0-9+/]*={0,3})$").unwrap();
+fn extract_auth_from_header(auth_header: String) -> Option<String> {
+    let re = Regex::new(r"^Basic\s(?P<encoded_credentials>[-A-Za-z0-9+/]*={0,3})$").unwrap();
 
-        if let Some(caps) = re.captures(&auth_string) {
-            match caps.name("encoded_credentials") {
-                Some(creds) => Some(creds.as_str().to_string()),
-                _ => None,
-            }
-        } else {
-            None
+    if let Some(caps) = re.captures(&auth_header) {
+        match caps.name("encoded_credentials") {
+            Some(creds) => Some(creds.as_str().to_string()),
+            _ => None,
         }
     } else {
         None
@@ -42,8 +34,8 @@ fn extract_auth_from_header(auth_header: Option<String>) -> Option<String> {
 
 fn extract_credentials_from_base64_string(encoded_credentials: String) -> Option<(String, String)> {
     if let Ok(credentials) = base64::decode(encoded_credentials) {
-        let re = Regex::new(r"^(?P<username>\w+)\:(?P<password>\w+)$").unwrap();
-        match re.captures(&encoded_credentials) {
+        let re = Regex::new(r"^(?P<username>\w+):(?P<password>\w+)$").unwrap();
+        match re.captures(std::str::from_utf8(&credentials).unwrap_or("")) {
             Some(caps) => {
                 if let (Some(username), Some(password)) =
                     (caps.name("username"), caps.name("password"))
@@ -60,18 +52,58 @@ fn extract_credentials_from_base64_string(encoded_credentials: String) -> Option
     }
 }
 
-pub fn basic_login(
-    auth_header: Option<String>,
-) -> impl Filter<Extract = (), Error = Rejection> + Copy {
-    if let Some(encoded_credentials) = extract_auth_from_header(auth_header) {
-        match extract_credentials_from_base64_string(encoded_credentials) {
-            Some((username, password)) => match auth_is_correct(username, password) {
-                true => (),
-                false => warp::reject::custom(AuthenticationFailed),
-            },
-            None => warp::reject::custom(CorruptAuthenticationPassed),
-        }
-    } else {
-        warp::reject::custom(NoAuthenticationPassed)
+#[cfg(test)]
+mod test {
+    use super::*;
+    use test_case::test_case;
+
+    #[test]
+    fn get_basic_username_works() {
+        std::env::set_var("BASIC_AUTH_USERNAME", "username");
+        assert_eq!(get_basic_username(), "username".to_string());
+        std::env::remove_var("BASIC_AUTH_USERNAME");
+    }
+
+    #[test]
+    fn get_basic_password_works() {
+        std::env::set_var("BASIC_AUTH_PASSWORD", "password");
+        assert_eq!(get_basic_password(), "password".to_string());
+        std::env::remove_var("BASIC_AUTH_PASSWORD");
+    }
+
+    #[test]
+    fn check_auth_works() {
+        std::env::set_var("BASIC_AUTH_USERNAME", "username");
+        std::env::set_var("BASIC_AUTH_PASSWORD", "password");
+        assert_eq!(
+            auth_is_correct("username".to_owned(), "password".to_owned()),
+            true
+        );
+        assert_eq!(
+            auth_is_correct("not_username".to_owned(), "password".to_owned()),
+            false
+        );
+        assert_eq!(
+            auth_is_correct("username".to_owned(), "not_password".to_owned()),
+            false
+        );
+        std::env::remove_var("BASIC_AUTH_USERNAME");
+        std::env::remove_var("BASIC_AUTH_PASSWORD");
+    }
+
+    #[test_case("Weird String", None)]
+    #[test_case("dXNlcm5hbWU6cGFzc3dvcmQ=", Some(("username".to_string(), "password".to_string())))]
+    fn extracting_credentials_works(input: &str, output: Option<(String, String)>) {
+        assert_eq!(
+            extract_credentials_from_base64_string(input.to_string()),
+            output
+        );
+    }
+
+    #[test_case("Basic abcdef", Some("abcdef".to_string()))]
+    #[test_case("Basic abcde===", Some("abcde===".to_string()))]
+    #[test_case("Not an auth string", None)]
+    fn extracting_base64_creds_works(input: &str, output: Option<String>) {
+        assert_eq!(extract_auth_from_header(input.to_string()), output);
     }
 }
