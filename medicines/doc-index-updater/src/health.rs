b0a4ffaf9fad4ddf8e00d8e408c5b82540aa587c
Matt Doughty <matt.doughty@mhra.gov.uk>
2020-03-23 13:55:36 +0000

    /healthz returns a blank response (204).

--- medicines/doc-index-updater/src/health.rs
+++ medicines/doc-index-updater/src/health.rs
@@ -1,34 +1,8 @@
-use warp::{
-    reply::{Json, Xml},
-    Filter, Rejection, Reply,
-};
-
-use std::collections::HashMap;
-
-fn get_health_handler() -> HashMap<String, bool> {
-    let mut result = HashMap::new();
-    result.insert("healthy".to_string(), true);
-
-    result
-}
-
-async fn get_health_handler_json() -> Result<Json, Rejection> {
-    Ok(warp::reply::json(&get_health_handler()))
-}
-
-async fn get_health_handler_xml() -> Result<Xml, Rejection> {
-    Ok(warp::reply::xml(&get_health_handler()))
-}
+use warp::{http::StatusCode, Filter, Rejection, Reply};
 
 pub fn get_health() -> impl Filter<Extract = impl Reply, Error = Rejection> + Copy {
     warp::path!("healthz")
         .and(warp::get())
-        .and_then(get_health_handler_json)
-}
-
-pub fn get_health_xml() -> impl Filter<Extract = impl Reply, Error = Rejection> + Copy {
-    warp::path!("healthz")
-        .and(warp::get())
-        .and(warp::header::exact_ignore_case("accept", "application/xml"))
-        .and_then(get_health_handler_xml)
+        .map(warp::reply)
+        .map(|reply| warp::reply::with_status(reply, StatusCode::NO_CONTENT))
 }
