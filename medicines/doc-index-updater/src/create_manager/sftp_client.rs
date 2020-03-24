7a4012ec85a9aa27aabd9cdad88945c6a88ba08a
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-03-24 15:45:39 +0000

    Custom error SentinelSftpError when Tcp or Ssh fails

--- medicines/doc-index-updater/src/create_manager/sftp_client.rs
+++ medicines/doc-index-updater/src/create_manager/sftp_client.rs
@@ -1,13 +1,50 @@
 use crate::models::FileSource;
-use ssh2::{Error, File, Session, Sftp};
-use std::{io::Read, net::TcpStream};
+use ssh2::{File, Session, Sftp};
+use std::{fmt::Display, io::Read, net::TcpStream};
 
-async fn sentinel_sftp_factory() -> Result<Sftp, Error> {
+#[derive(Debug)]
+enum SentinelSftpError {
+    TcpError(std::io::Error),
+    Ssh2Error(ssh2::Error),
+}
+
+impl Display for SentinelSftpError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SentinelSftpError::TcpError(e) => {
+                write!(f, "A TCP error connecting to server. ({:?})", e)
+            }
+            SentinelSftpError::Ssh2Error(e) => {
+                write!(f, "An SSH error connecting to server. ({:?})", e)
+            }
+        }
+    }
+}
+
+impl std::error::Error for SentinelSftpError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
+
+impl From<ssh2::Error> for SentinelSftpError {
+    fn from(e: ssh2::Error) -> Self {
+        SentinelSftpError::Ssh2Error(e)
+    }
+}
+
+impl From<std::io::Error> for SentinelSftpError {
+    fn from(e: std::io::Error) -> Self {
+        SentinelSftpError::TcpError(e)
+    }
+}
+
+async fn sentinel_sftp_factory() -> Result<Sftp, SentinelSftpError> {
     let server = get_env_fail_fast("SENTINEL_SFTP_SERVER").await;
     let user = get_env_fail_fast("SENTINEL_SFTP_USERNAME").await;
     let password = get_env_fail_fast("SENTINEL_SFTP_PASSWORD").await;
 
-    let tcp = TcpStream::connect(format!("{}:22", server)).unwrap();
+    let tcp = TcpStream::connect(format!("{}:22", server))?;
     let mut ssh_session = Session::new()?;
     ssh_session.set_tcp_stream(tcp);
     ssh_session.handshake()?;
