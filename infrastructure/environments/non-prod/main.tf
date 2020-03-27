debebcd5b4fde2f7861c711ce77b421711dbde0d
Stuart Harris <stuartharris@Stuarts-MBP.lan>
2020-03-27 11:22:34 +0000

    fix cpd storage account name

--- infrastructure/environments/non-prod/main.tf
+++ infrastructure/environments/non-prod/main.tf
@@ -19,10 +19,6 @@ terraform {
   }
 }
 
-locals {
-  namespace = "mhraproductsnonprod"
-}
-
 resource "azurerm_resource_group" "products" {
   name     = var.RESOURCE_GROUP_PRODUCTS
   location = var.REGION
@@ -38,7 +34,7 @@ module "products" {
 
   environment         = var.ENVIRONMENT
   location            = var.REGION
-  namespace           = local.namespace
+  namespace           = "mhraproductsnonprod"
   resource_group_name = azurerm_resource_group.products.name
 }
 
@@ -67,7 +63,7 @@ module cpd {
 
   environment         = var.ENVIRONMENT
   location            = var.REGION
-  namespace           = local.namespace
+  namespace           = "mhracpdnonprod"
   resource_group_name = azurerm_resource_group.products.name
 }
 
