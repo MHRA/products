9e4d41c9a225e8522ba2f7c19b4d568cbbed1190
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-04-01 15:21:14 +0100

    putting the development .env in a nice place

--- medicines/search/get-env-development.sh
+++ medicines/search/get-env-development.sh
@@ -1,5 +1,5 @@
 az keyvault secret show \
-  --vault-name mhra-non-prod \
-  --name search-index-creator-env \
+  --vault-name mhra-non-prod-02 \
+  --name search-index-creator-development-env \
   --query value \
   --output tsv
