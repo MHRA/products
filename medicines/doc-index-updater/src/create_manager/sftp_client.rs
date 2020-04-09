fd1fd38f6001bef1ae6dd805fe85453a2b98b467
Stuart Harris <stuartharris@Stuarts-MBP.lan>
2020-04-09 16:58:03 +0100

    use async sftp client

--- medicines/doc-index-updater/src/create_manager/sftp_client.rs
+++ medicines/doc-index-updater/src/create_manager/sftp_client.rs
@@ -1,15 +1,16 @@
 use crate::models::FileSource;
 use anyhow::anyhow;
-use ssh2::{File, Session, Sftp};
-use std::{io::Read, net::TcpStream};
+use async_ssh2::{Session, Sftp};
+use std::net::TcpStream;
 use thiserror::Error;
+use tokio::io::AsyncReadExt;
 
 #[derive(Error, Debug)]
 pub enum SftpError {
     #[error("A TCP error connecting to server. ({0:?})")]
     TcpError(#[from] std::io::Error),
     #[error("An SSH error connecting to server. ({0:?})")]
-    Ssh2Error(#[from] ssh2::Error),
+    Ssh2Error(#[from] async_ssh2::Error),
     #[error("File could not be retrieved on server")]
     CouldNotRetrieveFile,
     #[error(transparent)]
@@ -33,18 +34,18 @@ async fn sentinel_sftp_factory() -> Result<Sftp, SftpError> {
     tracing::debug!(message = "SFTP server connection established");
 
     let mut ssh_session = Session::new()?;
-    ssh_session.set_tcp_stream(tcp);
-    ssh_session.handshake()?;
+    ssh_session.set_tcp_stream(tcp)?;
+    ssh_session.handshake().await?;
 
     tracing::debug!(message = "SFTP server handshake complete");
 
     tracing::debug!(message = "Trying authentication");
-    ssh_session.userauth_password(&user, &password)?;
+    ssh_session.userauth_password(&user, &password).await?;
     tracing::debug!(message = "Finished trying authentication");
 
     if ssh_session.authenticated() {
         tracing::debug!(message = "SFTP session authenticated");
-        ssh_session.sftp().map_err(Into::into)
+        ssh_session.sftp().await.map_err(Into::into)
     } else {
         let message = "SFTP session authentication failed";
         tracing::debug!(message);
@@ -58,9 +59,9 @@ pub async fn get_env_fail_fast(name: &str) -> String {
 }
 
 async fn retrieve_file_from_sftp(
-    sftp: &mut ssh2::Sftp,
+    sftp: &mut Sftp,
     filepath: String,
-) -> Result<File, anyhow::Error> {
+) -> Result<async_ssh2::File, anyhow::Error> {
     let path = std::path::Path::new(&filepath);
 
     tracing::info!("{:?}", path);
@@ -70,7 +71,7 @@ async fn retrieve_file_from_sftp(
     let parent_dir = path.parent();
     if let Some(parent_dir_path) = parent_dir {
         tracing::debug!("Finding contents of {:?}", &parent_dir_path);
-        if let Ok(file_stats) = sftp.readdir(parent_dir_path) {
+        if let Ok(file_stats) = sftp.readdir(parent_dir_path).await {
             for (path_buf, file_stat) in file_stats {
                 tracing::debug!("{:?}", path_buf.to_str());
                 tracing::debug!("File stats: {:#?}", file_stat);
@@ -80,11 +81,14 @@ async fn retrieve_file_from_sftp(
         }
     }
 
-    Ok(sftp.open(path).map_err(|e| {
+    Ok(sftp.open(path).await.map_err(|e| {
         tracing::error!("{:?}", e);
-        match e.code() {
-            -31 => SftpError::CouldNotRetrieveFile,
-            _ => SftpError::Ssh2Error(e),
+        match e {
+            async_ssh2::Error::SSH2(e) => match e.code() {
+                -31 => SftpError::CouldNotRetrieveFile,
+                _ => SftpError::Ssh2Error(e.into()),
+            },
+            _ => SftpError::Ssh2Error(e.into()),
         }
     })?)
 }
@@ -95,7 +99,7 @@ pub async fn retrieve(source: FileSource, filepath: String) -> Result<Vec<u8>, S
     };
     let mut file = retrieve_file_from_sftp(&mut sentinel_sftp_client, filepath.clone()).await?;
     let mut bytes = Vec::<u8>::new();
-    let size = file.read_to_end(&mut bytes)?;
+    let size = file.read_to_end(&mut bytes).await?;
     tracing::info!("File retrieved from SFTP at {} ({} bytes) ", filepath, size);
     Ok(bytes)
 }
