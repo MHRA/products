f08c946b2d6b5989f3a0fb473fd31a332ddf5315
Stuart Harris <stuartharris@Stuarts-MBP.lan>
2020-04-19 14:02:25 +0100

    try using tide instead of warp

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,8 +1,11 @@
-use warp::Filter;
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    let mut app = tide::new();
+    app.at("/hello/:name").get(hello_handler);
+    app.listen("127.0.0.1:3030").await
+}
 
-#[tokio::main]
-async fn main() {
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, there {}!", name));
-
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+async fn hello_handler(req: tide::Request<()>) -> tide::Result<String> {
+    let name: String = req.param("name").expect("no name param");
+    Ok(format!("Hello, {}\n", name))
 }
