2560ebad0211cbc6cd21f1455679b64353261ff6
Stuart Harris <stuartharris@Stuarts-MBP.lan>
2020-04-20 16:43:36 +0100

    add unit tests, integration tests, readme

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,11 +1,4 @@
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
-    let mut app = tide::new();
-    app.at("/hello/:name").get(hello_handler);
-    app.listen("127.0.0.1:3030").await
-}
-
-async fn hello_handler(req: tide::Request<()>) -> tide::Result<String> {
-    let name: String = req.param("name").expect("no name param");
-    Ok(format!("Hello, {}\n", name))
+    hello_world::create_app().listen("0.0.0.0:3030").await
 }
