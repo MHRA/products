45c0f030c4bddaf6afe6e8c6a27e77cb9fc3f4f4
Sam Rowe <sam.rowe@red-badger.com>
2020-04-23 11:11:45 +0100

    Use proper graphql variables

--- medicines/web/src/services/graphql-loader.ts
+++ medicines/web/src/services/graphql-loader.ts
@@ -3,15 +3,29 @@ import { IProduct } from '../model/substance';
 
 const graphQlUrl = process.env.GRAPHQL_URL as string;
 
-const getProductsForSubstance = async (substanceName: any) => {
-  const query = `{ substance(name: "${substanceName}") { products { name, documentCount } } }`;
+const getProductsForSubstance = async (substanceName: string) => {
+  const query = `
+query ($substanceName: String) {
+  substance(name: $substanceName) {
+    products {
+      name
+      documentCount
+    }
+  }
+}`;
+
+  const variables = { substanceName };
+
   const response = await fetch(graphQlUrl, {
     method: 'POST',
     mode: 'cors',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ query, variables: null }),
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
   });
 
   return (await response.json()).data.substance.products;
