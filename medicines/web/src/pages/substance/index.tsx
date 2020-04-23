580cafd658bca2c2694e0157f15cf4bf1fe2a63f
Sam Rowe <sam.rowe@red-badger.com>
2020-04-23 11:43:30 +0100

    Rename graphql-loader -> products-loader

--- medicines/web/src/pages/substance/index.tsx
+++ medicines/web/src/pages/substance/index.tsx
@@ -11,7 +11,7 @@ import {
 import { useLocalStorage } from '../../hooks';
 import { IProduct } from '../../model/substance';
 import Events from '../../services/events';
-import { products } from '../../services/graphql-loader';
+import { products } from '../../services/products-loader';
 import substanceLoader from '../../services/substance-loader';
 
 const azureProductsLoader = async (substance: string) => {
