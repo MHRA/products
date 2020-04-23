ebafe2739a227ae51f604926ea610fba40030c0e
Andy Davis <andy.davis@red-badger.com>
2020-04-23 14:45:01 +0100

    Script to update *nix hosts file for local dev

---
+++ infrastructure/scripts/update-hosts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+sh ./update-kubernetes-config.sh
+
+cat /etc/hosts | sed "/.*argocd.test.mhra.gov.uk.*$/d" > /tmp/hosts
+kubectl get services --namespace istio-system istio-ingressgateway -o json \
+    | jq '.status.loadBalancer.ingress[0].ip' \
+    | sed -e "s!\"\([^\"]*\)\"!\1       argocd.test.mhra.gov.uk doc-index-updater.test.mhra.gov.uk medicines-api.test.mhra.gov.uk!" \
+    | cat >> /tmp/hosts
+
+cp /etc/hosts /etc/hosts.old
+cp /tmp/hosts /etc/hosts
