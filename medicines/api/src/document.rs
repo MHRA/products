b5c4b10870b5ed827738680da598fe841407d7c8
Craig Anderson <craiga@craiga.id.au>
2020-04-27 17:31:33 +0100

    Use number of results returned from Azure to determine pagination, not the number of results requested.

--- medicines/api/src/document.rs
+++ medicines/api/src/document.rs
@@ -49,7 +49,7 @@ pub async fn get_documents(
             let bytes = base64::decode(after)?;
             let string = std::str::from_utf8(&bytes)?;
             string.parse::<i32>()? + 1
-        },
+        }
         None => 0,
     };
 
@@ -79,9 +79,10 @@ pub async fn get_documents(
         })
         .collect();
 
+    let result_count = azure_result.search_results.len() as i32;
     let total_count = azure_result.count.unwrap_or(0);
     let has_previous_page = offset != 0;
-    let has_next_page = offset + result_count <= total_count;
+    let has_next_page = offset + result_count < total_count;
     let start_cursor = base64::encode(offset.to_string());
     let end_cursor =
         base64::encode(std::cmp::min(total_count, offset + result_count - 1).to_string());
@@ -142,44 +143,48 @@ mod test {
         }
     }
 
-    fn given_some_search_results() -> Vec<IndexResult> {
+    fn given_a_search_result(product_name: &str) -> IndexResult {
+        IndexResult {
+            product_name: Some(product_name.to_string()),
+            doc_type: "Spc".to_string(),
+            file_name: "our_id".to_string(),
+            metadata_storage_name: "storage_name".to_string(),
+            metadata_storage_path: "test/path".to_string(),
+            substance_name: vec!["substance".to_string()],
+            title: "title".to_string(),
+            created: None,
+            facets: vec!["facet".to_string()],
+            keywords: None,
+            metadata_storage_size: 300,
+            release_state: None,
+            rev_label: None,
+            suggestions: vec!["suggestion".to_string()],
+            score: 1.0,
+            highlights: None,
+        }
+    }
+
+    fn given_first_page_of_search_results() -> Vec<IndexResult> {
         vec![
-            IndexResult {
-                doc_type: "Spc".to_string(),
-                file_name: "our_id".to_string(),
-                metadata_storage_name: "storage_name".to_string(),
-                metadata_storage_path: "test/path".to_string(),
-                product_name: Some("product".to_string()),
-                substance_name: vec!["substance".to_string()],
-                title: "title".to_string(),
-                created: None,
-                facets: vec!["facet".to_string()],
-                keywords: None,
-                metadata_storage_size: 300,
-                release_state: None,
-                rev_label: None,
-                suggestions: vec!["suggestion".to_string()],
-                score: 1.0,
-                highlights: None,
-            },
-            IndexResult {
-                doc_type: "Par".to_string(),
-                file_name: "other_id".to_string(),
-                metadata_storage_name: "other_storage_name".to_string(),
-                metadata_storage_path: "test/other_path".to_string(),
-                product_name: Some("product two".to_string()),
-                substance_name: vec!["ecnatsbus".to_string()],
-                title: "Jaws".to_string(),
-                created: None,
-                facets: vec!["otherfacet".to_string()],
-                keywords: None,
-                metadata_storage_size: 300,
-                release_state: None,
-                rev_label: None,
-                suggestions: vec!["the other document".to_string()],
-                score: 0.9,
-                highlights: None,
-            },
+            given_a_search_result("first"),
+            given_a_search_result("second"),
+            given_a_search_result("third"),
+            given_a_search_result("fourth"),
+            given_a_search_result("fifth"),
+            given_a_search_result("sixth"),
+            given_a_search_result("seventh"),
+            given_a_search_result("eighth"),
+            given_a_search_result("ninth"),
+            given_a_search_result("tenth"),
+        ]
+    }
+
+    fn given_last_page_of_search_results() -> Vec<IndexResult> {
+        vec![
+            given_a_search_result("fourth last"),
+            given_a_search_result("third last"),
+            given_a_search_result("second last"),
+            given_a_search_result("last"),
         ]
     }
 
@@ -209,23 +214,19 @@ mod test {
         .unwrap()
     }
 
-    fn then_we_have_expected_documents(
-        documents_response: &Documents,
-        search_results: &Vec<IndexResult>,
-    ) {
-        assert_eq!(documents_response.edges.len(), search_results.len());
-        let expected_names = search_results
-            .into_iter()
-            .map(|result| result.product_name.to_owned().unwrap());
+    fn then_we_have_the_first_page(documents_response: &Documents) {
+        let expected_names = vec![
+            "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+            "tenth",
+        ];
         let edges = &documents_response.edges;
         let actual_names = edges
             .into_iter()
-            .map(|edge| edge.node.product_name.as_ref().unwrap().as_ref());
-        assert!(expected_names.eq(actual_names));
-    }
+            .map(|edge| edge.node.product_name.as_ref().unwrap());
+        assert!(actual_names.eq(expected_names));
 
-    fn then_we_are_on_first_page(documents_response: &Documents) {
         assert_eq!(1234, documents_response.total_count);
+
         let expected_page_info = PageInfo {
             has_previous_page: false,
             has_next_page: true,
@@ -235,32 +236,38 @@ mod test {
         assert_eq!(expected_page_info, documents_response.page_info);
     }
 
-    fn then_we_are_on_last_page(documents_response: &Documents) {
+    fn then_we_have_the_last_page(documents_response: &Documents) {
+        let expected_names = vec!["fourth last", "third last", "second last", "last"];
+        let edges = &documents_response.edges;
+        let actual_names = edges
+            .into_iter()
+            .map(|edge| edge.node.product_name.as_ref().unwrap());
+
+        assert!(actual_names.eq(expected_names));
+
         assert_eq!(1234, documents_response.total_count);
         let expected_page_info = PageInfo {
             has_previous_page: true,
             has_next_page: false,
             start_cursor: base64::encode("1230"),
-            end_cursor: base64::encode("1234"),
+            end_cursor: base64::encode("1233"),
         };
         assert_eq!(expected_page_info, documents_response.page_info);
     }
 
     #[test]
     fn test_get_documents_first_page() {
-        let search_results = given_some_search_results();
+        let search_results = given_first_page_of_search_results();
         let search_client = given_a_search_client(&search_results);
         let response = when_we_get_the_first_page_of_documents(search_client);
-        then_we_have_expected_documents(&response, &search_results);
-        then_we_are_on_first_page(&response);
+        then_we_have_the_first_page(&response);
     }
 
     #[test]
     fn test_get_documents_last_page() {
-        let search_results = given_some_search_results();
+        let search_results = given_last_page_of_search_results();
         let search_client = given_a_search_client(&search_results);
         let response = when_we_get_the_last_page_of_documents(search_client);
-        then_we_have_expected_documents(&response, &search_results);
-        then_we_are_on_last_page(&response);
+        then_we_have_the_last_page(&response);
     }
 }
