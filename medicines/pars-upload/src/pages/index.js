d678de697dc20e8d25c22829cbb53b6e29c0a1e6
Sam Rowe <sam.rowe@red-badger.com>
2020-05-07 12:19:32 +0100

    Link to new PAR page from home page

--- medicines/pars-upload/src/pages/index.js
+++ medicines/pars-upload/src/pages/index.js
@@ -1,5 +1,12 @@
 import { H1 } from "../typography";
+import { ButtonLink } from "../button";
 
-const Index = () => <H1>Public Assessment Reports (PARs) upload</H1>;
+const Index = () => (
+  <>
+    <H1>Public Assessment Reports (PARs) upload</H1>
+
+    <ButtonLink href="/new-par">Upload a new document</ButtonLink>
+  </>
+);
 
 export default Index;
