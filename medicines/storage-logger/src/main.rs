2c67e67456b3acaa9ad070e8ac76d2f9c8ede955
Tim Lee <tim.lee@red-badger.com>
2020-05-09 18:04:34 +0100

    Working fetcher and uploader

--- medicines/storage-logger/src/main.rs
+++ medicines/storage-logger/src/main.rs
@@ -1,7 +1,11 @@
+#[macro_use]
+extern crate lazy_static;
 use azure_sdk_core::prelude::*;
 use azure_sdk_storage_blob::Blob;
 use azure_sdk_storage_core::prelude::Client;
+use chrono::{DateTime, Utc};
 use futures::stream::StreamExt;
+use regex::Regex;
 use std::error::Error;
 
 #[tokio::main]
@@ -10,10 +14,14 @@ async fn main() {
 }
 
 async fn get_blob_contents() -> Result<(), Box<dyn Error>> {
-    let account = std::env::var("STORAGE_ACCOUNT").expect("Set env variable STORAGE_ACCOUNT first!");
-    let master_key = std::env::var("STORAGE_MASTER_KEY").expect("Set env variable STORAGE_MASTER_KEY first!");
-    let container_name = std::env::var("STORAGE_CONTAINER_NAME").expect("Set env variable STORAGE_MASTER_KEY first!");
-    let contents_log_container_name = std::env::var("STORAGE_CONTAINER_BACKUP_NAME").expect("Set env variable STORAGE_MASTER_KEY first!");
+    let account =
+        std::env::var("STORAGE_ACCOUNT").expect("Set env variable STORAGE_ACCOUNT first!");
+    let master_key =
+        std::env::var("STORAGE_MASTER_KEY").expect("Set env variable STORAGE_MASTER_KEY first!");
+    let container_name = std::env::var("STORAGE_CONTAINER_NAME")
+        .expect("Set env variable STORAGE_MASTER_KEY first!");
+    let contents_log_container_name = std::env::var("STORAGE_CONTAINER_BACKUP_NAME")
+        .expect("Set env variable STORAGE_MASTER_KEY first!");
 
     let client = Client::new(&account, &master_key)?;
 
@@ -31,58 +39,68 @@ async fn get_blob_contents() -> Result<(), Box<dyn Error>> {
     while let Some(value) = stream.next().await {
         // let len = value?.incomplete_vector.len();
         for cont in value?.incomplete_vector.iter() {
-            if count > 3 {
+            if count > 2 {
                 break;
             }
 
-            let con = if let Some(file_name) = cont.metadata.get("file_name") {
-                file_name.to_owned()
-            } else {
-                String::from("")
-            };
-
-            let pls = if let Some(pls) = cont.metadata.get("pl_number") {
-                pls.to_owned()
-            } else {
-                String::from("")
+            let con = match cont.metadata.get("file_name") {
+                Some(file_name) => file_name.to_owned(),
+                None => String::from(""),
             };
 
             let created = cont.creation_time.to_string();
 
-            let modified = if let Some(date) = cont.last_modified {
-                date.to_string()
-            } else {
-                String::from("")
+            let modified = match cont.last_modified {
+                Some(date) => date.to_string(),
+                None => String::from(""),
             };
 
-            blob_list.push(format!(
-                "{}, {}, {}, {}, {}",
-                cont.name, con, pls, created, modified
-            ));
+            let pls = match cont.metadata.get("pl_number") {
+                Some(pls_string) => get_pls_vec_from_string(pls_string),
+                None => vec![String::from("")],
+            };
+
+            for pl in pls {
+                blob_list.push(format!(
+                    "{}, {}, {}, {}, {}",
+                    cont.name, con, pl, created, modified
+                ));
+            }
+
             count += 1;
         }
     }
 
-    // construct blob name to include date
-    // save file first before uploading as blob or find way to convert to file data
-    // also include content md5
+    println!("{:?}", blob_list);
     // extract out into methods
     // add tests
     // find how to run in git workflow
+    // let blobs_as_string = blob_list.join("\n");
+    // let file_data = blobs_as_string.as_bytes();
+    // let file_digest = md5::compute(&file_data[..]);
 
-    let blob_name = client
-        .put_block_blob()
-        .with_container_name(&contents_log_container_name)
-        .with_blob_name(&name)
-        .with_content_type("application/pdf")
-        .with_metadata(&metadata_ref)
-        .with_body(&file_data[..])
-        .with_content_md5(&file_digest[..])
-        .finalize()
-        .await
-        .map_err(|e| anyhow!("Couldn't upload to blob storage: {:?}", e))?;
+    // let now: DateTime<Utc> = Utc::now();
+    // let blob_name = now.format("docs-content-log-%Y-%m-%d.csv").to_string();
 
-    println!("{:?}", blob_list);
+    // client
+    //     .put_block_blob()
+    //     .with_container_name(&contents_log_container_name)
+    //     .with_blob_name(&blob_name)
+    //     .with_content_type("text/csv")
+    //     .with_body(&file_data[..])
+    //     .with_content_md5(&file_digest[..])
+    //     .finalize()
+    //     .await?;
 
     Ok(())
+}
+
+fn get_pls_vec_from_string(pl_string: &str) -> Vec<String> {
+    lazy_static! {
+        static ref RE_PL: Regex = Regex::new(r"(PL|PLPI|THR)[0-9]+").unwrap();
+    }
+    RE_PL
+        .captures_iter(pl_string)
+        .map(|cap| cap[0].to_string())
+        .collect()
 }
