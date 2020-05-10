77082219d859c92b1708930e669137f0e4f5d349
Tim Lee <tim.lee@red-badger.com>
2020-05-10 12:58:05 +0100

    Add tests and workflow file

--- medicines/storage-logger/src/main.rs
+++ medicines/storage-logger/src/main.rs
@@ -10,7 +10,7 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() {
-    create_blob_log().await;
+    let _ = create_blob_log().await;
 }
 
 async fn create_blob_log() -> Result<(), Box<dyn Error>> {
@@ -87,10 +87,7 @@ fn extract_blob_strings(blob: &Blob) -> Vec<String> {
     blob_strings
 }
 
-async fn write_to_log_store(
-    client: &Client,
-    blob_list: Vec<String>,
-) -> Result<(), AzureError> {
+async fn write_to_log_store(client: &Client, blob_list: Vec<String>) -> Result<(), AzureError> {
     let contents_log_container_name = std::env::var("STORAGE_CONTAINER_BACKUP_NAME")
         .expect("Set env variable STORAGE_MASTER_KEY first!");
 
@@ -122,4 +119,91 @@ fn get_pls_vec_from_string(pl_string: &str) -> Vec<String> {
         .captures_iter(pl_string)
         .map(|cap| cap[0].to_string())
         .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use azure_sdk_core::lease::LeaseState;
+    use azure_sdk_storage_blob::blob::BlobType;
+    use std::collections::HashMap;
+    use test_case::test_case;
+
+    #[test_case(
+        &"[\"PL1234567\",\"PLPI987654321\", \"THR54321678\"]",
+        vec![String::from("PL1234567"), String::from("PLPI987654321"), String::from("THR54321678")]
+    )]
+    #[test_case(
+        &"[\"POL1234567\",\"12345678\", \"THR 54321678\"]",
+        vec![]
+    )]
+    #[test_case(
+        &"",
+        vec![]
+    )]
+    fn test_get_pls_vec_from_string(input: &str, expected: Vec<String>) {
+        let actual = get_pls_vec_from_string(input);
+        assert_eq!(actual.len(), expected.len());
+        println!("{:?}", &actual);
+        for i in 0..actual.len() {
+            assert_eq!(actual[i], expected[i]);
+        }
+    }
+
+    fn get_test_blob(name: String, container_name: String, date: DateTime<Utc>, pl: String, file_name: String) -> Blob {
+        let mut metadata: HashMap<String,String> = HashMap::new();
+        metadata.insert(String::from("pl_number"), String::from(format!("[\"{}\"]", pl)));
+        metadata.insert(String::from("file_name"), file_name);
+        Blob {
+            name,
+            container_name,
+            snapshot_time: None,
+            creation_time: date,
+            last_modified: Some(date),
+            etag: None,
+            content_language: None,
+            content_length: 5,
+            content_type: None,
+            content_encoding: None,
+            content_md5: None,
+            cache_control: None,
+            content_disposition: None,
+            x_ms_blob_sequence_number: None,
+            blob_type: BlobType::BlockBlob,
+            access_tier: None,
+            lease_status: None,
+            lease_state: LeaseState::Available,
+            lease_duration: None,
+            copy_id: None,
+            copy_status: None,
+            copy_source: None,
+            copy_progress: None,
+            copy_completion_time: None,
+            copy_status_description: None,
+            incremental_copy: None,
+            server_encrypted: false,
+            access_tier_inferred: None,
+            access_tier_change_time: None,
+            deleted_time: None,
+            remaining_retention_days: None,
+            metadata: metadata,
+        }
+    }
+
+    #[test]
+    fn test_get_blob_string() {
+        let date = chrono::DateTime::<Utc>::from(
+            DateTime::parse_from_rfc3339("1996-12-19T16:39:57-08:00").unwrap(),
+        );
+        let name = String::from("test_blog");
+        let container_name = String::from("test_container");
+        let pl = String::from("PL1234567");
+        let file_name = String::from("CON1234567");
+        let blob = get_test_blob(name, container_name, date, pl, file_name);
+        let expected = String::from("test_blog, CON1234567, PL1234567, 1996-12-20 00:39:57 UTC, 1996-12-20 00:39:57 UTC");
+        
+        let actual = extract_blob_strings(&blob);
+
+        assert_eq!(actual[0], expected);
+    }
 }
