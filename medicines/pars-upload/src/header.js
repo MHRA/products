50f37be358986ba0f6921b74bd3d1254b1acc3ee
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2020-05-11 16:36:00 +0100

    sr-only

--- medicines/pars-upload/src/header.js
+++ medicines/pars-upload/src/header.js
@@ -1,18 +1,19 @@
-import { MhraLogo } from "./mhra_logo";
+import { MhraLogo } from './mhra_logo';
 
 export const Header = () => (
   <header
-    style={{ background: "none" }}
+    style={{ background: 'none' }}
     className="govuk-header"
     role="banner"
     data-module="govuk-header"
   >
     <div
-      style={{ borderBottomColor: "#0F1290" }}
+      style={{ borderBottomColor: '#0F1290' }}
       className="govuk-header__container govuk-width-container"
     >
       <div className="govuk-header__logo">
         <a href="/" className="govuk-header__link govuk-header__link--homepage">
+          <span className="sr-only">MHRA PARs upload homepage</span>
           <span className="govuk-header__logotype">
             <MhraLogo />
           </span>
