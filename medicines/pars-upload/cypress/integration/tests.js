9978a0d38eb7f02778b09c7b1e9897ec4bcca44f
Sam Rowe <sam.rowe@red-badger.com>
2020-05-12 16:57:30 +0100

    Update tests

--- medicines/pars-upload/cypress/integration/tests.js
+++ medicines/pars-upload/cypress/integration/tests.js
@@ -1,8 +1,13 @@
 describe('PARs upload homepage', function () {
   it('can get to the form page', function () {
     cy.server();
+
     cy.visit('/');
-    cy.contains('Upload a new document').click();
-    cy.get("input[name='product_names']").type('Craig');
+
+    cy.findByText('Upload a new document').click();
+
+    cy.findByText('Continue').click();
+
+    cy.findByText('New Public Assessment Report').should('exist');
   });
 });
--- medicines/pars-upload/src/back-link.js
+++ medicines/pars-upload/src/back-link.js
@@ -2,6 +2,6 @@ import Link from 'next/link';
 
 export const BackLink = ({ href }) => (
   <Link href={href}>
-    <a class="govuk-back-link">Back</a>
+    <a className="govuk-back-link">Back</a>
   </Link>
 );
