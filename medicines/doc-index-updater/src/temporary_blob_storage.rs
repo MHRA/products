54b25ace37deabdf7f7c1c082e797c5c0f463370
Anubha Singh <45950125+anubha22@users.noreply.github.com>
2020-05-15 14:14:27 +0100

    new temp blob

--- medicines/doc-index-updater/src/temporary_blob_storage.rs
+++ medicines/doc-index-updater/src/temporary_blob_storage.rs
@@ -5,6 +5,7 @@ use crate::{
 use async_trait::async_trait;
 use azure_sdk_core::{
     BlobNameSupport, BodySupport, ContainerNameSupport, ContentMD5Support, ContentTypeSupport,
+    MetadataSupport,
 };
 use azure_sdk_storage_blob::Blob;
 use storage_client::GetBlob;
@@ -58,7 +59,11 @@ impl Default for TemporaryBlobStorage {
 
 #[async_trait]
 impl StorageClient for TemporaryBlobStorage {
-    async fn add_file(self, file_data: &[u8]) -> Result<StorageFile, StorageClientError> {
+    async fn add_file(
+        self,
+        file_data: &[u8],
+        metadata_ref: HashMap<&str, &str>,
+    ) -> Result<StorageFile, StorageClientError> {
         let storage_client = storage_client_factory()?.azure_client;
 
         let file_digest = md5::compute(&file_data[..]);
@@ -69,6 +74,7 @@ impl StorageClient for TemporaryBlobStorage {
             .with_container_name(&self.container_name)
             .with_blob_name(&name)
             .with_content_type("application/pdf")
+            .with_metadata(&metadata_ref)
             .with_body(&file_data[..])
             .with_content_md5(&file_digest[..])
             .finalize()
