108522ec351bb86ad1dde4d9f9de12730420e481
Andy Davis <andy.davis@red-badger.com>
2020-05-19 12:14:00 +0100

    Get reply url from config

--- infrastructure/modules/products/app-registrations.tf
+++ infrastructure/modules/products/app-registrations.tf
@@ -1,6 +1,6 @@
 resource "azuread_application" "pars-upload" {
   name                    = "pars-upload-${var.namespace}"
-  reply_urls              = ["http://${var.namespace}"] #don't think we need this
+  reply_urls              = ["http://localhost:3000", "https://${var.namespace}"] #this url needs to be correct for the environment
   group_membership_claims = "All"
   homepage                = "http://pars-upload-${var.namespace}" #don't think we need this
 
