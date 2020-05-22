b8de6fe81b250583f642585fe0eaac3c1ad28c5b
Sam Rowe <sam.rowe@red-badger.com>
2020-05-22 11:48:40 +0100

    Update review PAR upload screen

--- medicines/pars-upload/src/review_submission.js
+++ medicines/pars-upload/src/review_submission.js
@@ -1,21 +1,26 @@
 import { Layout } from './layout'
-import { H1, Para } from './typography'
+import { H1, Para, H3 } from './typography'
 import { Button } from './button'
 import { BackLink } from './back-link'
 import { ErrorSummary } from './error_summary'
+import { SummaryListWithoutActions } from './summary_list'
 
 export const ReviewSubmission = ({
   steps,
   currentStepIndex,
   submit,
   submissionError,
+  goBack,
 }) => {
   const pageData = steps.slice(0, currentStepIndex)
 
-  console.log('XXX', pageData)
+  const goToPrevPage = (event) => {
+    event.preventDefault()
+    goBack()
+  }
 
   return (
-    <Layout intro={<BackLink href="/" />}>
+    <Layout intro={<BackLink href="/" onClick={goToPrevPage} />}>
       {submissionError ? (
         <ErrorSummary>
           <Para>An unexpected error occurred submitting the form.</Para>
@@ -25,9 +30,74 @@ export const ReviewSubmission = ({
 
       <H1>Check your answers before sending the report</H1>
 
+      {pageData.map(({ type, data }, i) => {
+        const key = `${type}-${i}`
+
+        switch (type) {
+          case 'product':
+            return <ProductSummary key={key} data={data} />
+          case 'file':
+            return <FileSummary key={key} data={data} />
+          default:
+            return null
+        }
+      })}
+
       <Button type="button" onClick={submit}>
-        Continue
+        Accept and send
       </Button>
     </Layout>
   )
 }
+
+const ProductSummary = ({ data }) => (
+  <SummaryWrapper title={data.get('title')}>
+    <SummaryListWithoutActions
+      items={[
+        {
+          key: 'Brand/Generic name',
+          value: data.get('product_name'),
+        },
+        {
+          key: 'Strength',
+          value: data.get('strength'),
+        },
+        {
+          key: 'Pharmaceutical dose form',
+          value: data.get('pharmaceutical_dose'),
+        },
+        {
+          key: `Active substance${
+            data.getAll('active_substance').length > 1 ? 's' : ''
+          }`,
+          value: data.getAll('active_substance').join(', '),
+        },
+        {
+          key: 'License number',
+          value: data.get('license_number'),
+        },
+      ]}
+    />
+  </SummaryWrapper>
+)
+
+const FileSummary = ({ data }) => (
+  <SummaryWrapper title="Document">
+    <SummaryListWithoutActions
+      items={[
+        {
+          key: 'Document name',
+          value: data.get('file').name,
+        },
+      ]}
+    />
+  </SummaryWrapper>
+)
+
+const SummaryWrapper = ({ title, children }) => (
+  <div className="govuk-!-margin-bottom-9">
+    <H3 component="h2">{title}</H3>
+
+    {children}
+  </div>
+)
--- medicines/pars-upload/src/typography.js
+++ medicines/pars-upload/src/typography.js
@@ -1,17 +1,17 @@
-export const H1 = ({ children }) => (
-  <h1 className="govuk-heading-xl">{children}</h1>
+export const H1 = ({ children, component: Component = 'h3' }) => (
+  <Component className="govuk-heading-xl">{children}</Component>
 )
 
-export const H2 = ({ children }) => (
-  <h2 className="govuk-heading-l">{children}</h2>
+export const H2 = ({ children, component: Component = 'h3' }) => (
+  <Component className="govuk-heading-l">{children}</Component>
 )
 
-export const H3 = ({ children }) => (
-  <h3 className="govuk-heading-m">{children}</h3>
+export const H3 = ({ children, component: Component = 'h3' }) => (
+  <Component className="govuk-heading-m">{children}</Component>
 )
 
-export const H4 = ({ children }) => (
-  <h4 className="govuk-heading-s">{children}</h4>
+export const H4 = ({ children, component: Component = 'h3' }) => (
+  <Component className="govuk-heading-s">{children}</Component>
 )
 
 export const Para = ({ children }) => <p className="govuk-body">{children}</p>
