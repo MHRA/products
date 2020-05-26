eab26f37ff59b79ab837670372f8fa68536d4626
Andy Davis <github@andy-davis.co.uk>
2020-05-26 15:16:50 +0100

    Updated the implict workflow in terraform in order to use the proper getToken call

--- infrastructure/modules/products/app-registrations.tf
+++ infrastructure/modules/products/app-registrations.tf
@@ -1,9 +1,9 @@
 resource "azuread_application" "pars-upload" {
   name                    = "pars-upload-${var.environment}"
   reply_urls              = concat(["https://${azurerm_cdn_endpoint.pars.host_name}"], var.add_local_pars_reply_url ? ["http://localhost:3000"] : [])
-  group_membership_claims = "All"
+  group_membership_claims = "SecurityGroup"
   homepage                = "https://${azurerm_cdn_endpoint.pars.host_name}" #don't think we need this
-
+  oauth2_allow_implicit_flow = true
   app_role {
     allowed_member_types = [
       "User",
