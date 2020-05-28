c5ceb9a73ece867cab0ba0c57aa8b9ea548ef4fb
Andy Davis <github@andy-davis.co.uk>
2020-05-28 16:37:08 +0100

    add primary webhost too

--- infrastructure/modules/products/app-registrations.tf
+++ infrastructure/modules/products/app-registrations.tf
@@ -1,8 +1,7 @@
 resource "azuread_application" "pars-upload" {
   name                    = "pars-upload-${var.environment}"
-  reply_urls              = concat(["https://${azurerm_cdn_endpoint.pars.host_name}"], var.add_local_pars_reply_url ? ["http://localhost:3000"] : [])
+  reply_urls              = concat(["https://${azurerm_cdn_endpoint.pars.host_name}", "https://${azurerm_storage_account.pars.primary_web_host}"], var.add_local_pars_reply_url ? ["http://localhost:3000"] : [])
   group_membership_claims = "SecurityGroup"
-  homepage                = "https://${azurerm_cdn_endpoint.pars.host_name}" #don't think we need this
   oauth2_allow_implicit_flow = true
   owners = var.app_registration_owners
   app_role {
