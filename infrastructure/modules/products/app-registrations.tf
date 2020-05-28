ed219d2f5e650af7c772abdf096c97936c03e309
Andy Davis <github@andy-davis.co.uk>
2020-05-28 16:38:22 +0100

    Temporarily allow non prod to work for all of the app registrations

--- infrastructure/modules/products/app-registrations.tf
+++ infrastructure/modules/products/app-registrations.tf
@@ -1,6 +1,6 @@
 resource "azuread_application" "pars-upload" {
   name                    = "pars-upload-${var.environment}"
-  reply_urls              = concat(["https://${azurerm_cdn_endpoint.pars.host_name}", "https://${azurerm_storage_account.pars.primary_web_host}"], var.add_local_pars_reply_url ? ["http://localhost:3000"] : [])
+  reply_urls              = concat(["https://${azurerm_cdn_endpoint.pars.host_name}", "https://${azurerm_storage_account.pars.primary_web_host}", "https://mhraparsnonprod.z33.web.core.windows.net", "https://mhraparsnon-prod.azureedge.net"], var.add_local_pars_reply_url ? ["http://localhost:3000"] : [])
   group_membership_claims = "SecurityGroup"
   oauth2_allow_implicit_flow = true
   owners = var.app_registration_owners
