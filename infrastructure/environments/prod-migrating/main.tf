da5122f40e097ce47766b301cb603cd06f3feb92
Tim Lee <tim.lee@red-badger.com>
2020-05-28 14:29:02 +0100

    Include new prod tf dir

---
+++ infrastructure/environments/prod-migrating/main.tf
@@ -0,0 +1,174 @@
+provider "azurerm" {
+  version = "=2.8.0"
+  features {}
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "mhraprodtfstate"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+
+resource "random_integer" "deployment" {
+  min = 1000
+  max = 9999
+}
+
+locals {
+  namespace        = "mhraproducts${random_integer.deployment.result}"
+  cpd_namespace    = "mhracpd${random_integer.deployment.result}"
+  pars_namespace   = "mhrapars${random_integer.deployment.result}"
+  service_bus_name = "doc-index-updater-${random_integer.deployment.result}"
+}
+
+resource "azurerm_resource_group" "products" {
+  name     = var.RESOURCE_GROUP_PRODUCTS
+  location = var.REGION
+
+  tags = {
+    environment = var.ENVIRONMENT
+  }
+}
+
+resource "azurerm_resource_group" "keyvault" {
+  name     = var.KEYVAULT_RESOURCE_GROUP
+  location = var.REGION
+
+  tags = {
+    environment = var.ENVIRONMENT
+  }
+}
+
+resource "azurerm_route_table" "load_balancer" {
+  name                = "aparz-spoke-rt-products-internal-only"
+  location            = azurerm_resource_group.products.location
+  resource_group_name = azurerm_resource_group.products.name
+
+  tags = {
+    environment = var.ENVIRONMENT
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "load_balancer" {
+  subnet_id      = azurerm_subnet.load_balancer.id
+  route_table_id = azurerm_route_table.load_balancer.id
+}
+
+resource "azurerm_virtual_network" "cluster" {
+  name                = "aparz-spoke-pd-products"
+  location            = var.REGION
+  resource_group_name = azurerm_resource_group.products.name
+  address_space       = ["10.5.67.0/24"]
+}
+
+resource "azurerm_subnet" "load_balancer" {
+  name                 = "aparz-spoke-products-sn-01"
+  address_prefixes     = ["10.5.67.0/26"]
+  resource_group_name  = azurerm_virtual_network.cluster.resource_group_name
+  virtual_network_name = azurerm_virtual_network.cluster.name
+}
+
+# Website
+module "products" {
+  source = "../../modules/products"
+
+  environment             = var.ENVIRONMENT
+  location                = var.REGION
+  namespace               = local.namespace
+  pars_namespace          = local.pars_namespace
+  resource_group_name     = azurerm_resource_group.products.name
+  search_sku              = "standard"
+  app_registration_owners = var.KEYVAULT_AUTHORISED_PERSON_IDS
+  pars_app_name           = "pars-upload"
+}
+
+# website
+module "products_web" {
+  source = "../../modules/products-web"
+
+  namespace            = local.namespace
+  environment          = var.ENVIRONMENT
+  storage_account_name = module.products.storage_account_name
+  resource_group_name  = azurerm_resource_group.products.name
+  origin_host_name     = module.products.storage_account_primary_web_host
+}
+
+# CPD
+module cpd {
+  source = "../../modules/cpd"
+
+  environment         = var.ENVIRONMENT
+  location            = var.REGION
+  namespace           = local.cpd_namespace
+  resource_group_name = azurerm_resource_group.products.name
+}
+
+# Logs
+module logs {
+  source = "../../modules/logs"
+
+  namespace           = local.namespace
+  environment         = var.ENVIRONMENT
+  location            = var.REGION
+  resource_group_name = azurerm_resource_group.products.name
+}
+
+# AKS
+module cluster {
+  source = "../../modules/cluster"
+
+  client_id                             = var.CLIENT_ID
+  client_secret                         = var.CLIENT_SECRET
+  environment                           = var.ENVIRONMENT
+  location                              = var.REGION
+  resource_group_name                   = azurerm_resource_group.products.name
+  vnet_name                             = azurerm_virtual_network.cluster.name
+  vnet_resource_group                   = azurerm_virtual_network.cluster.resource_group_name
+  lb_subnet_id                          = azurerm_subnet.load_balancer.id
+  cluster_subnet_name                   = "aparz-spoke-products-sn-02"
+  cluster_subnet_cidr                   = "10.5.67.64/26"
+  cluster_route_destination_cidr_blocks = var.CLUSTER_ROUTE_DESTINATION_CIDR_BLOCKS
+  cluster_route_next_hop                = var.CLUSTER_ROUTE_NEXT_HOP
+  lb_route_table_id                     = azurerm_route_table.load_balancer.id
+  default_node_count                    = "3"
+  support_email_addresses               = var.SUPPORT_EMAIL_ADDRESSES
+  log_cluster_diagnostics               = true
+  logs_storage_account_id               = module.logs.logs_resource_group_id
+}
+
+data "azurerm_public_ip" "external" {
+  name                = split("/", module.cluster.load_balancer_public_outbound_ip_id)[8]
+  resource_group_name = split("/", module.cluster.load_balancer_public_outbound_ip_id)[4]
+}
+
+# Service Bus
+module service_bus {
+  source = "../../modules/service-bus"
+
+  environment         = var.ENVIRONMENT
+  location            = var.REGION
+  name                = local.service_bus_name
+  resource_group_name = azurerm_resource_group.products.name
+  redis_use_firewall  = true
+  redis_firewall_ip   = data.azurerm_public_ip.external.ip_address
+}
+
+# Key vault
+module keyvault {
+  source = "../../modules/keyvault"
+
+  environment                 = var.ENVIRONMENT
+  location                    = var.REGION
+  name                        = local.namespace
+  resource_group_name         = azurerm_resource_group.keyvault.name
+  access_CIDR                 = var.KEYVAULT_ACCESS_CIDR_BLOCKS
+  authorised_person_ids       = var.KEYVAULT_AUTHORISED_PERSON_IDS
+  network_acls_default_action = "Deny"
+}
---
+++ infrastructure/environments/prod-migrating/variables.tf
@@ -0,0 +1,55 @@
+variable "REGION" {
+  description = "Azure region"
+  default     = "uksouth"
+}
+
+variable "RESOURCE_GROUP_PRODUCTS" {
+  description = "Products resource group name"
+  default     = "apazr-rg-1001"
+}
+
+variable "CLIENT_ID" {
+  description = "Service Principal Client ID"
+}
+
+variable "CLIENT_SECRET" {
+  description = "Service Principal Client Secret"
+}
+
+variable "ENVIRONMENT" {
+  description = "Environment name"
+  default     = "production"
+}
+
+variable "CLUSTER_ROUTE_DESTINATION_CIDR_BLOCKS" {
+  type        = list(string)
+  description = "CIDR block destination for default route"
+  default     = []
+}
+
+variable "CLUSTER_ROUTE_NEXT_HOP" {
+  description = "Next hop for default route"
+}
+
+variable "SUPPORT_EMAIL_ADDRESSES" {
+  type        = list(string)
+  description = "A list of email addresses for first line support alerts to be sent to."
+  default     = []
+}
+
+variable "KEYVAULT_ACCESS_CIDR_BLOCKS" {
+  type        = list(string)
+  description = "CIDR blocks representing whitelisted IPs to access keyvault"
+  default     = []
+}
+
+variable "KEYVAULT_AUTHORISED_PERSON_IDS" {
+  type        = list(string)
+  description = "IDs of objects (people etc) to associate access policies for"
+  default     = []
+}
+
+variable "KEYVAULT_RESOURCE_GROUP" {
+  description = "Name of resource group where keyvault is deployed"
+  default     = "secrets"
+}
