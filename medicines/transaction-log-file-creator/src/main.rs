ff57513b8f417bca0001a4757b0c705434364c3f
Tim Lee <tim.lee@red-badger.com>
2020-05-28 18:26:02 +0100

    Add header preventing blob overwrite

--- medicines/transaction-log-file-creator/src/main.rs
+++ medicines/transaction-log-file-creator/src/main.rs
@@ -1,11 +1,10 @@
 extern crate lazy_static;
 use anyhow::anyhow;
-use azure_sdk_core::errors::AzureError;
+use azure_sdk_core::modify_conditions::IfMatchCondition;
 use azure_sdk_core::prelude::*;
 use azure_sdk_storage_blob::prelude::*;
 use azure_sdk_storage_core::prelude::Client;
 use chrono::{DateTime, Utc};
-use hyper::StatusCode;
 
 #[tokio::main]
 async fn main() {
@@ -21,7 +20,8 @@ async fn main() {
 async fn create_log_file() -> Result<(), anyhow::Error> {
     let client = get_client()?;
     let blob_name = get_log_file_name(Utc::now());
-    create_append_blob(&client, blob_name).await
+    let log_container_name = get_log_container_name();
+    create_append_blob(&client, &blob_name, &log_container_name).await
 }
 
 fn get_client() -> Result<Client, anyhow::Error> {
@@ -39,65 +39,36 @@ fn get_log_file_name(date: DateTime<Utc>) -> String {
     date.format("file-change-log-%Y-%m").to_string()
 }
 
-async fn create_append_blob(client: &Client, blob_name: String) -> Result<(), anyhow::Error> {
-    let log_container_name = std::env::var("LOG_STORAGE_CONTAINER")
-        .expect("Set env variable LOG_STORAGE_CONTAINER first!");
+fn get_log_container_name() -> String {
+    std::env::var("LOG_STORAGE_CONTAINER").expect("Set env variable LOG_STORAGE_CONTAINER first!")
+}
 
-    if !should_create_blob(client, &blob_name, &log_container_name).await {
-        return Err(anyhow!("Didn't create blob - it may already exist"));
-    }
+async fn create_append_blob(
+    client: &Client,
+    blob_name: &str,
+    container_name: &str,
+) -> Result<(), anyhow::Error> {
+    let match_condition_preventing_blob_overwrite = IfMatchCondition::NotMatch("*");
 
     client
         .put_append_blob()
-        .with_container_name(&log_container_name)
-        .with_blob_name(&blob_name)
+        .with_container_name(container_name)
+        .with_blob_name(blob_name)
         .with_content_type("text/csv")
+        .with_if_match_condition(match_condition_preventing_blob_overwrite)
         .finalize()
         .await
         .map_err(|e| {
             eprintln!("Error creating append blob: {:?}", e);
-            anyhow!("Couldn't create append blob")
+            anyhow!("Couldn't create append blob: {:?}")
         })?;
 
     Ok(())
 }
 
-async fn should_create_blob(client: &Client, blob_name: &str, container_name: &str) -> bool {
-    let get_blob_exists_result = get_if_file_exists(client, blob_name, container_name).await;
-    result_is_file_does_not_exist_error(get_blob_exists_result)
-}
-
-async fn get_if_file_exists(
-    client: &Client,
-    blob_name: &str,
-    container_name: &str,
-) -> Result<bool, AzureError> {
-    client
-        .get_blob()
-        .with_container_name(container_name)
-        .with_blob_name(blob_name)
-        .finalize()
-        .await
-        .map(|_response| true)
-}
-
-fn result_is_file_does_not_exist_error(result: Result<bool, AzureError>) -> bool {
-    match result {
-        Err(AzureError::UnexpectedHTTPResult(e)) => e.status_code() == StatusCode::NOT_FOUND,
-        Err(e) => {
-            eprintln!("Error getting file: {:?}", e);
-            false
-        }
-        _ => false,
-    }
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
-    use azure_sdk_core::errors::UnexpectedHTTPResult;
-    use hyper::StatusCode;
-    use test_case::test_case;
 
     #[test]
     fn test_get_log_file_name() {
@@ -106,32 +77,5 @@ mod test {
         );
         let log_file_name = get_log_file_name(date);
         assert_eq!(log_file_name, "file-change-log-1996-12".to_string());
-    }
-
-    fn get_file_not_found_error() -> AzureError {
-        AzureError::UnexpectedHTTPResult(UnexpectedHTTPResult::new(
-            StatusCode::from_u16(200).unwrap(),
-            StatusCode::from_u16(404).unwrap(),
-            &"",
-        ))
-    }
-
-    fn get_other_error() -> AzureError {
-        AzureError::UnexpectedHTTPResult(UnexpectedHTTPResult::new(
-            StatusCode::from_u16(200).unwrap(),
-            StatusCode::from_u16(400).unwrap(),
-            &"",
-        ))
-    }
-
-    #[test_case(Err(get_file_not_found_error()), true)]
-    #[test_case(Err(get_other_error()), false)]
-    #[test_case(Ok(true), false)]
-    fn check_result_returns_true_when_file_does_not_exist(
-        input: Result<bool, AzureError>,
-        expected_output: bool,
-    ) {
-        let result = result_is_file_does_not_exist_error(input);
-        assert_eq!(result, expected_output);
     }
 }
