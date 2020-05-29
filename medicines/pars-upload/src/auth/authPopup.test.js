cef2c7a9d76067498782d3387f97ed61ad67883e
Andy Davis <github@andy-davis.co.uk>
2020-05-29 14:45:45 +0100

    re order

--- medicines/pars-upload/src/auth/authPopup.test.js
+++ medicines/pars-upload/src/auth/authPopup.test.js
@@ -1,13 +1,13 @@
 const popup = require('./authPopup')
 
-test('extracts base url from url with port', () => {
-  expect(popup.getCurrentHost('http://localhost:3000/1/2/3/4/5')).toBe(
-    'http://localhost:3000'
-  )
-})
-
 test('extracts base url from url', () => {
   expect(popup.getCurrentHost('http://theserver/1/2/3/4/5')).toBe(
     'http://theserver'
+  )
+})
+
+test('extracts base url from url with port', () => {
+  expect(popup.getCurrentHost('http://localhost:3000/1/2/3/4/5')).toBe(
+    'http://localhost:3000'
   )
 })
