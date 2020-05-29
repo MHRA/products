ca010fbb2e8c49a6105ee055ed59e4db9d0152f5
Andy Davis <github@andy-davis.co.uk>
2020-05-29 14:44:51 +0100

    extract base url when no port

--- medicines/pars-upload/src/auth/authPopup.test.js
+++ medicines/pars-upload/src/auth/authPopup.test.js
@@ -5,3 +5,9 @@ test('extracts base url from url with port', () => {
     'http://localhost:3000'
   )
 })
+
+test('extracts base url from url', () => {
+  expect(popup.getCurrentHost('http://theserver/1/2/3/4/5')).toBe(
+    'http://theserver'
+  )
+})
