cab773d5972b3aeea70df2d369b9624058fa6c58
Sam Rowe <sam.rowe@red-badger.com>
2020-05-29 15:25:27 +0100

    Use .js file ending

---
+++ medicines/pars-upload/src/footer.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Footer } from './footer'
+
+describe(Footer, () => {
+  it('should render', () => {
+    const component = shallow(<Footer />)
+    expect(component).toMatchSnapshot()
+  })
+})
