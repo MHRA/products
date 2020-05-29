d5e573d692baee5ca5da30a18841312c4fdf06f0
Andy Davis <github@andy-davis.co.uk>
2020-05-29 15:08:07 +0100

    change to data driven test with table

--- medicines/pars-upload/src/auth/authPopup.test.js
+++ medicines/pars-upload/src/auth/authPopup.test.js
@@ -1,25 +1,12 @@
-const popup = require('./authPopup')
+const authPopup = require('./authPopup')
 
-test('extracts base url from url', () => {
-  expect(popup.getCurrentHost('http://theserver/1/2/3/4/5')).toBe(
-    'http://theserver'
-  )
-})
-
-test('extracts base url from a url with port and path', () => {
-  expect(popup.getCurrentHost('http://localhost:3000/1/2/3/4/5')).toBe(
-    'http://localhost:3000'
-  )
-})
-
-test('extracts base url from a url without path', () => {
-  expect(popup.getCurrentHost('http://theserver:8000')).toBe(
-    'http://theserver:8000'
-  )
-})
-
-test('extracts base url from a url without path or port', () => {
-  expect(popup.getCurrentHost('http://theserver.com')).toBe(
-    'http://theserver.com'
-  )
+describe.each([
+  ['https://website', 'https://website/1/2/3/4/5'],
+  ['http://localhost:4000', 'http://localhost:4000/1/2/3/4/5'],
+  ['http://website', 'http://website'],
+  ['https://website.com', 'https://website.com/1/2/3/4/5'],
+])('', (expected, input) => {
+  test(`getCurrentHost for ${input} returns ${expected}`, () => {
+    expect(authPopup.getCurrentHost(input)).toBe(expected)
+  })
 })
