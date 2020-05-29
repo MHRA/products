909becbe8c73775bdc052cf1ec7f56d8538f6a32
Andy Davis <github@andy-davis.co.uk>
2020-05-29 15:09:59 +0100

    flip actual and expected

--- medicines/pars-upload/src/auth/authPopup.test.js
+++ medicines/pars-upload/src/auth/authPopup.test.js
@@ -1,11 +1,11 @@
 const authPopup = require('./authPopup')
 
 describe.each([
-  ['https://website', 'https://website/1/2/3/4/5'],
-  ['http://localhost:4000', 'http://localhost:4000/1/2/3/4/5'],
+  ['https://website/1/2/3/4/5', 'https://website'],
+  ['http://localhost:4000/1/2/3/4/5', 'http://localhost:4000'],
   ['http://website', 'http://website'],
-  ['https://website.com', 'https://website.com/1/2/3/4/5'],
-])('', (expected, input) => {
+  ['https://website.com/1/2/3/4/5', 'https://website.com'],
+])('', (input, expected) => {
   test(`getCurrentHost for ${input} returns ${expected}`, () => {
     expect(authPopup.getCurrentHost(input)).toBe(expected)
   })
