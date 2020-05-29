5fe9af99a669f52f4e7083be1ca0a696c9f12b2c
Andy Davis <github@andy-davis.co.uk>
2020-05-29 14:48:17 +0100

    test without path but with port

--- medicines/pars-upload/src/auth/authPopup.test.js
+++ medicines/pars-upload/src/auth/authPopup.test.js
@@ -6,8 +6,14 @@ test('extracts base url from url', () => {
   )
 })
 
-test('extracts base url from url with port', () => {
+test('extracts base url from a url with port and path', () => {
   expect(popup.getCurrentHost('http://localhost:3000/1/2/3/4/5')).toBe(
     'http://localhost:3000'
+  )
+})
+
+test('extracts base url from a url without path', () => {
+  expect(popup.getCurrentHost('http://theserver:8000')).toBe(
+    'http://theserver:8000'
   )
 })
