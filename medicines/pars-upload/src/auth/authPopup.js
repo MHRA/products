091c5cf9c5eaeadce7ae777491243085a58a3d97
Andy Davis <github@andy-davis.co.uk>
2020-05-29 14:41:59 +0100

    pass the window href in to make function more pure

--- medicines/pars-upload/src/auth/authPopup.js
+++ medicines/pars-upload/src/auth/authPopup.js
@@ -2,7 +2,7 @@ import { UserAgentApplication } from 'msal'
 import { msalConfig, loginRequest } from './authConfig'
 
 export async function getAccount() {
-  msalConfig.auth.redirectUri = getCurrentHost()
+  msalConfig.auth.redirectUri = getCurrentHost(window.location.href)
   const msalInstance = new UserAgentApplication(msalConfig)
   const account = msalInstance.getAccount()
 
@@ -23,13 +23,13 @@ export async function getAccount() {
   }
 }
 
-function getCurrentHost() {
-  var uriComponents = window.location.href.split('/')
+export function getCurrentHost(currentFullUrl) {
+  var uriComponents = currentFullUrl.split('/')
   return uriComponents[0] + '//' + uriComponents[2]
 }
 
 export function signIn() {
-  msalConfig.auth.redirectUri = getCurrentHost()
+  msalConfig.auth.redirectUri = getCurrentHost(window.location.href)
   return new UserAgentApplication(msalConfig)
     .loginPopup(loginRequest)
     .then(getAccount)
