aad9d22e864c5132b9785473076dd7be9390dff3
Tim Lee <tim.lee@red-badger.com>
2020-06-30 14:28:32 +0100

    Revert learning site changes

--- learning/web/src/utils/mdx-components.js
+++ learning/web/src/utils/mdx-components.js
@@ -7,18 +7,10 @@ const StyledH1 = styled.h1`
   color: ${black};
 `
 
-const StyledSup = styled.sup`
-  & + sup {
-    padding-left: 10px;
-  }
-`
-
 const H1 = props => <StyledH1 {...props} />
 const A = props => <Link to={props.href} {...props} />
-const Sup = props => <StyledSup {...props} />
 
 export const components = {
   h1: H1,
   a: A,
-  sup: Sup,
 }
