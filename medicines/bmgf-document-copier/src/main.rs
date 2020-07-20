ad4b17775dd5020a63522c3ea12a430d376a3bf5
Tim Lee <tim.lee@red-badger.com>
2020-07-20 09:57:16 +0100

    Neaten

--- medicines/bmgf-document-copier/src/main.rs
+++ medicines/bmgf-document-copier/src/main.rs
@@ -7,8 +7,9 @@ use std::{error::Error, sync::Arc};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let blobs_list = get_blobs_list(Arc::new(get_client()?)).await?;
-    copy_blobs(Arc::new(get_client()?), blobs_list).await?;
+    let client = Arc::new(get_client()?);
+    let blobs_list = get_blobs_list(&client).await?;
+    copy_blobs(&client, blobs_list).await?;
     println!("Completed successfully");
     Ok(())
 }
@@ -32,7 +33,7 @@ fn get_copy_source_base_url() -> String {
     )
 }
 
-async fn get_blobs_list(client: Arc<dyn Client>) -> Result<Vec<String>, AzureError> {
+async fn get_blobs_list(client: &Arc<Box<dyn Client>>) -> Result<Vec<String>, AzureError> {
     let container_name = std::env::var("SOURCE_CONTAINER_NAME")
         .expect("Set env variable SOURCE_CONTAINER_NAME first!");
 
@@ -54,13 +55,16 @@ async fn get_blobs_list(client: Arc<dyn Client>) -> Result<Vec<String>, AzureErr
     Ok(blob_list)
 }
 
-async fn copy_blobs(client: Arc<dyn Client>, blob_list: Vec<String>) -> Result<(), AzureError> {
+async fn copy_blobs(
+    client: &Arc<Box<dyn Client>>,
+    blob_list: Vec<String>,
+) -> Result<(), AzureError> {
     let destination = "$web";
     let copy_source_base_url = get_copy_source_base_url();
 
     for blob in blob_list {
         let source_url = &format!("{}/{}", copy_source_base_url, &blob);
-        let blob_name = &format!("content/A%20great%20report/{}", &blob);
+        let blob_name = &format!("content/{}", &blob);
         client
             .copy_blob_from_url()
             .with_container_name(&destination)
