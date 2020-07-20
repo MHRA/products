dd521a5d9b0ae3642a49847d4e8a6d06a12e2687
Tim Lee <tim.lee@red-badger.com>
2020-07-20 18:32:17 +0100

    Hacky getStaticProps working

--- medicines/web/src/pages/bmgf/[report].tsx
+++ medicines/web/src/pages/bmgf/[report].tsx
@@ -1,6 +1,9 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
+import matter from 'gray-matter';
+import glob from 'node-glob';
+import ReactMarkdown from 'react-markdown';
 import marked from 'marked';
 import Page from '../../components/page';
 // import SearchResults from '../../components/search-results';
@@ -23,90 +26,147 @@ import {
 } from '../../services/querystring-interpreter';
 // import { convertResults } from '../../services/results-converter';
 
-// const pageSize = 10;
-// const productPath = '/product';
+// // const pageSize = 10;
+// // const productPath = '/product';
+
+// // interface IProductResult {
+// //   name: string;
+// //   count: number;
+// //   documents: IDocument[];
+// // }
+
+// // interface IProductPageInfo {
+// //   name: string;
+// //   page: number;
+// //   docTypes: DocType[];
+// // }
 
 // interface IProductResult {
-//   name: string;
 //   count: number;
-//   documents: IDocument[];
+//   documents: IBmgfSearchResult[];
 // }
 
-// interface IProductPageInfo {
-//   name: string;
-//   page: number;
-//   docTypes: DocType[];
-// }
-
-interface IProductResult {
-  count: number;
-  documents: IBmgfSearchResult[];
-}
-
-const azureDocumentsLoader = async (
-  report: string,
-): Promise<IProductResult> => {
-  const results = await getBmgfDocs(report);
-  return {
-    count: results.resultCount,
-    documents: results.results,
-  };
-};
-
-// const graphQlProductLoader = async ({
-//   name,
-//   page,
-//   docTypes,
-// }: IProductPageInfo): Promise<IProductResult> => {
-//   return documents.load({ name, page, pageSize, docTypes });
+// const azureDocumentsLoader = async (
+//   report: string,
+// ): Promise<IProductResult> => {
+//   const results = await getBmgfDocs(report);
+//   return {
+//     count: results.resultCount,
+//     documents: results.results,
+//   };
 // };
 
-const App: NextPage = () => {
-  const [storageAllowed, setStorageAllowed] = useLocalStorage(
-    'allowStorage',
-    false,
-  );
-  const [content, setContent] = React.useState();
-  // const [productName, setProductName] = React.useState('');
-  // const [count, setCount] = React.useState(0);
-  // const [pageNumber, setPageNumber] = React.useState(1);
-  // const [docTypes, setDocTypes] = React.useState<DocType[]>([]);
-  const [disclaimerAgree, setDisclaimerAgree] = React.useState(false);
-  // const [isLoading, setIsLoading] = React.useState(true);
+// // const graphQlProductLoader = async ({
+// //   name,
+// //   page,
+// //   docTypes,
+// // }: IProductPageInfo): Promise<IProductResult> => {
+// //   return documents.load({ name, page, pageSize, docTypes });
+// // };
 
-  const router = useRouter();
-  const {
-    query: { report, disclaimer: disclaimerQS },
-  } = router;
-  console.log(report);
+// const App: NextPage = () => {
+//   const [storageAllowed, setStorageAllowed] = useLocalStorage(
+//     'allowStorage',
+//     false,
+//   );
+//   const [content, setContent] = React.useState();
+//   // const [productName, setProductName] = React.useState('');
+//   // const [count, setCount] = React.useState(0);
+//   // const [pageNumber, setPageNumber] = React.useState(1);
+//   // const [docTypes, setDocTypes] = React.useState<DocType[]>([]);
+//   const [disclaimerAgree, setDisclaimerAgree] = React.useState(false);
+//   // const [isLoading, setIsLoading] = React.useState(true);
 
-  useEffect(() => {
-    if (!report || !report.length) {
-      return;
-    }
-    setDisclaimerAgree(parseDisclaimerAgree(disclaimerQS));
-    (async () => {
-      if (document) {
-        getMarkdownDoc('/about.md')
-          .then(function(response) {
-            return response.text();
-          })
-          .then(function(data) {
-            setContent(marked(data));
-          });
-      }
-    })();
-  }, [report, disclaimerQS]);
+//   const router = useRouter();
+//   const {
+//     query: { report, disclaimer: disclaimerQS },
+//   } = router;
+//   console.log(report);
 
+//   useEffect(() => {
+//     if (!report || !report.length) {
+//       return;
+//     }
+//     setDisclaimerAgree(parseDisclaimerAgree(disclaimerQS));
+//     (async () => {
+//       if (document) {
+//         getMarkdownDoc('/about.md')
+//           .then(function(response) {
+//             return response.text();
+//           })
+//           .then(function(data) {
+//             setContent(marked(data));
+//           });
+//       }
+//     })();
+//   }, [report, disclaimerQS]);
+
+//   return (
+//     <Page
+//       title="Products"
+//       storageAllowed={storageAllowed}
+//       setStorageAllowed={setStorageAllowed}
+//     >
+//       <article dangerouslySetInnerHTML={{ __html: content }}></article>
+//     </Page>
+//   );
+// };
+
+// export default App;
+
+function Report({ content }) {
   return (
-    <Page
-      title="Products"
-      storageAllowed={storageAllowed}
-      setStorageAllowed={setStorageAllowed}
-    >
+    <div>
       <article dangerouslySetInnerHTML={{ __html: content }}></article>
-    </Page>
+    </div>
   );
-};
+}
 
-export default App;
+export async function getStaticProps(context) {
+  const reportName = context.params;
+  console.log(reportName);
+  // @ts-ignore
+  const content = await import('../../content/about.md');
+  console.log('CONTENT!');
+  console.log(content);
+  console.log('MATTERED');
+  console.log(matter(content.default));
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      content: matter(content.default).content,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  //get all .md files in the posts dir
+  const reports = ['../../content/about.md']; // glob.sync('../../content/**/*.md');
+
+  //remove path and extension to leave filename only
+  const reportNames = reports.map(file =>
+    file
+      .split('/')[1]
+      .replace(/ /g, '-')
+      .slice(0, -3)
+      .trim(),
+  );
+  console.log(reportNames);
+  // create paths with `slug` param
+  const paths = reportNames.map(report => {
+    return {
+      params: {
+        report: `about`,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default Report;
