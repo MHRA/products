15899d73f1d4fa12ddf6ec0561dcbfd689160d75
Tim Lee <tim.lee@red-badger.com>
2020-08-17 11:20:01 +0100

    Update cypress tests to use new feature toggle

---
+++ medicines/web/scripts/test-e2e-azure-search.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+USE_GRAPHQL=false yarn dev &
+wait-on http://localhost:3000
+server_pid=$!
+ENV_VARS=$(cat .env | sed '/^$/d' | tr "\n" "," | sed 's/,$/ /g')
+yarn run cypress run --env $ENV_VARS --spec cypress/integration/azure_search_tests.js
+test_exit_code=$?
+kill $server_pid
+exit $test_exit_code
