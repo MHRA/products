58b4a84a70c0092209d839b09411cc7817c7ad30
Tim Lee <tim.lee@red-badger.com>
2020-08-19 09:57:13 +0100

    Launch on different ports

--- medicines/web/scripts/test-e2e-azure-search.sh
+++ medicines/web/scripts/test-e2e-azure-search.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-cross-env USE_GRAPHQL=false yarn dev &
-wait-on http://localhost:3000
+port=3000
+cross-env USE_GRAPHQL=false yarn dev -- -p $port &
+wait-on http://localhost:$port
 server_pid=$!
 ENV_VARS=$(cat .env | sed '/^$/d' | tr "\n" "," | sed 's/,$/ /g')
-yarn run cypress run --env $ENV_VARS --spec cypress/integration/azure_search_tests.js
+yarn run cypress run --env $ENV_VARS --spec cypress/integration/azure_search_tests.js --config baseUrl=http://localhost:$port
 test_exit_code=$?
 kill $server_pid
 exit $test_exit_code
