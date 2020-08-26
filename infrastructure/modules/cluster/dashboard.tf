96fc625f3395dfb62b1feb47add0a2fc45478ebb
Tim Lee <tim.lee@red-badger.com>
2020-08-26 15:49:51 +0100

    Manually updated dashboard config

--- infrastructure/modules/cluster/dashboard.tf
+++ infrastructure/modules/cluster/dashboard.tf
@@ -580,8 +580,6 @@ resource "azurerm_dashboard" "doc-index-updater-dashboard" {
             }
           }
         },
-
-
         "6": {
           "position": {
             "x": 0,
@@ -602,7 +600,7 @@ resource "azurerm_dashboard" "doc-index-updater-dashboard" {
               },
               {
                 "name": "Query",
-                "value": "InsightsMetrics\n| where Namespace == \"prometheus\"\n| where Name == \"job:error_response_latency_milliseconds_per_pod:mean\" \n| where parse_json(Tags).namespace == \"doc-index-updater\"\n| summarize avg(Val) by tostring(parse_json(Tags).pod_name), bin(TimeGenerated, 15s)\n| render timechart\n"
+                "value": "let messageContains = \"Successfully added\"\n;let clusterId = 'providers/Microsoft.ContainerService/managedClusters/non-prod';\nlet ContainerIdList = KubePodInventory\n| where ContainerName contains 'doc-index-updater'\n| where ClusterId contains clusterId\n| distinct ContainerID;\nContainerLog\n| where ContainerID in (ContainerIdList)\n| project LogEntrySource, LogEntry, TimeGenerated, Computer, Image, Name, ContainerID\n| order by TimeGenerated desc\n| render table\n| where parse_json(tostring(parse_json(LogEntry).fields)).message contains messageContains\n| summarize count() by bin(TimeGenerated, 15s)\n| render timechart\n"
               },
               {
                 "name": "TimeRange",
@@ -631,7 +629,7 @@ resource "azurerm_dashboard" "doc-index-updater-dashboard" {
               },
               {
                 "name": "PartId",
-                "value": "14fbaca6-515b-42e1-9e14-7392ad21d3f2"
+                "value": "0a3d5244-4ea2-4eb5-bf6b-694d1848df0a"
               },
               {
                 "name": "PartTitle",
@@ -661,7 +659,7 @@ resource "azurerm_dashboard" "doc-index-updater-dashboard" {
             "type": "Extension/AppInsightsExtension/PartType/AnalyticsPart",
             "settings": {
               "content": {
-                "PartTitle": "Error request latency (ms)",
+                "PartTitle": "Doc index updater successful uploads",
                 "PartSubTitle": "${azurerm_log_analytics_workspace.cluster.name}"
               }
             },
@@ -671,9 +669,273 @@ resource "azurerm_dashboard" "doc-index-updater-dashboard" {
             }
           }
         },
-
-        
-
+        "7": {
+          "position": {
+            "x": 6,
+            "y": 12,
+            "colSpan": 6,
+            "rowSpan": 4
+          },
+          "metadata": {
+            "inputs": [
+              {
+                "name": "ComponentId",
+                "value": {
+                  "SubscriptionId": "${data.azurerm_subscription.current.subscription_id}",
+                  "ResourceGroup": "${var.resource_group_name}",
+                  "Name": "${azurerm_log_analytics_workspace.cluster.name}",
+                  "ResourceId": "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourcegroups/${var.resource_group_name}/providers/microsoft.operationalinsights/workspaces/${azurerm_log_analytics_workspace.cluster.name}"
+                }
+              },
+              {
+                "name": "Query",
+                "value": "let messageContains = \"Successfully deleted\";\nlet ContainerIdList = KubePodInventory\n| where ContainerName contains 'doc-index-updater'\n| distinct ContainerID;\nContainerLog\n| where ContainerID in (ContainerIdList)\n| project LogEntrySource, LogEntry, TimeGenerated, Computer, Image, Name, ContainerID\n| order by TimeGenerated desc\n| render table\n| where parse_json(tostring(parse_json(LogEntry).fields)).message contains messageContains\n| summarize count() by bin(TimeGenerated, 15s)\n| render timechart\n"
+              },
+              {
+                "name": "TimeRange",
+                "value": "PT30M"
+              },
+              {
+                "name": "Dimensions",
+                "value": {
+                  "xAxis": {
+                    "name": "TimeGenerated",
+                    "type": "datetime"
+                  },
+                  "yAxis": [
+                    {
+                      "name": "count_",
+                      "type": "long"
+                    }
+                  ],
+                  "splitBy": [],
+                  "aggregation": "Sum"
+                }
+              },
+              {
+                "name": "Version",
+                "value": "1.0"
+              },
+              {
+                "name": "PartId",
+                "value": "108bc676-1974-4e64-a210-cab9fcb0535c"
+              },
+              {
+                "name": "PartTitle",
+                "value": "Analytics"
+              },
+              {
+                "name": "PartSubTitle",
+                "value": "${azurerm_log_analytics_workspace.cluster.name}"
+              },
+              {
+                "name": "resourceTypeMode",
+                "value": "workspace"
+              },
+              {
+                "name": "ControlType",
+                "value": "AnalyticsChart"
+              },
+              {
+                "name": "SpecificChart",
+                "value": "Line"
+              },
+              {
+                "name": "DashboardId",
+                "isOptional": true
+              }
+            ],
+            "type": "Extension/AppInsightsExtension/PartType/AnalyticsPart",
+            "settings": {
+              "content": {
+                "PartTitle": "Doc index updater successful deletes",
+                "PartSubTitle": "${azurerm_log_analytics_workspace.cluster.name}"
+              }
+            },
+            "asset": {
+              "idInputName": "ComponentId",
+              "type": "ApplicationInsights"
+            }
+          }
+        },
+        "8": {
+          "position": {
+            "x": 0,
+            "y": 16,
+            "colSpan": 6,
+            "rowSpan": 4
+          },
+          "metadata": {
+            "inputs": [
+              {
+                "name": "ComponentId",
+                "value": {
+                  "SubscriptionId": "${data.azurerm_subscription.current.subscription_id}",
+                  "ResourceGroup": "${var.resource_group_name}",
+                  "Name": "${azurerm_log_analytics_workspace.cluster.name}",
+                  "ResourceId": "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourcegroups/${var.resource_group_name}/providers/microsoft.operationalinsights/workspaces/${azurerm_log_analytics_workspace.cluster.name}"
+                }
+              },
+              {
+                "name": "Query",
+                "value": "let ContainerIdList = KubePodInventory\n| where ContainerName contains 'doc-index-updater'\n| distinct ContainerID;\nContainerLog\n| where ContainerID in (ContainerIdList)\n| project LogEntrySource, LogEntry, TimeGenerated, Computer, Image, Name, ContainerID\n| order by TimeGenerated desc\n| render table\n| extend message_ = tostring(parse_json(tostring(parse_json(LogEntry).fields)).message) \n| where parse_json(LogEntry).level == \"ERROR\"\n| summarize count() by bin(TimeGenerated, 15s)\n| render timechart\n"
+              },
+              {
+                "name": "TimeRange",
+                "value": "PT30M"
+              },
+              {
+                "name": "Dimensions",
+                "value": {
+                  "xAxis": {
+                    "name": "TimeGenerated",
+                    "type": "datetime"
+                  },
+                  "yAxis": [
+                    {
+                      "name": "count_",
+                      "type": "long"
+                    }
+                  ],
+                  "splitBy": [],
+                  "aggregation": "Sum"
+                }
+              },
+              {
+                "name": "Version",
+                "value": "1.0"
+              },
+              {
+                "name": "PartId",
+                "value": "50e6e7b1-8139-4368-802f-45cb615553d2"
+              },
+              {
+                "name": "PartTitle",
+                "value": "Analytics"
+              },
+              {
+                "name": "PartSubTitle",
+                "value": "${azurerm_log_analytics_workspace.cluster.name}"
+              },
+              {
+                "name": "resourceTypeMode",
+                "value": "workspace"
+              },
+              {
+                "name": "ControlType",
+                "value": "AnalyticsChart"
+              },
+              {
+                "name": "SpecificChart",
+                "value": "Line"
+              },
+              {
+                "name": "DashboardId",
+                "isOptional": true
+              }
+            ],
+            "type": "Extension/AppInsightsExtension/PartType/AnalyticsPart",
+            "settings": {
+              "content": {
+                "PartTitle": "Doc index updater server errors",
+                "PartSubTitle": "${azurerm_log_analytics_workspace.cluster.name}"
+              }
+            },
+            "asset": {
+              "idInputName": "ComponentId",
+              "type": "ApplicationInsights"
+            }
+          }
+        },
+        "9": {
+          "position": {
+            "x": 6,
+            "y": 16,
+            "colSpan": 6,
+            "rowSpan": 4
+          },
+          "metadata": {
+            "inputs": [
+              {
+                "name": "ComponentId",
+                "value": {
+                  "SubscriptionId": "${data.azurerm_subscription.current.subscription_id}",
+                  "ResourceGroup": "${var.resource_group_name}",
+                  "Name": "${azurerm_log_analytics_workspace.cluster.name}",
+                  "ResourceId": "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourcegroups/${var.resource_group_name}/providers/microsoft.operationalinsights/workspaces/${azurerm_log_analytics_workspace.cluster.name}"
+                }
+              },
+              {
+                "name": "Query",
+                "value": "let ContainerIdList = KubePodInventory\n| where ContainerName contains 'doc-index-updater'\n| distinct ContainerID;\nContainerLog\n| where ContainerID in (ContainerIdList)\n| project LogEntrySource, LogEntry, TimeGenerated, Computer, Image, Name, ContainerID\n| order by TimeGenerated desc\n| render table\n| where parse_json(LogEntry).level == \"ERROR\"\n| project TimeGenerated, tostring(parse_json(tostring(parse_json(LogEntry).fields)).message)\n"
+              },
+              {
+                "name": "TimeRange",
+                "value": "PT30M"
+              },
+              {
+                "name": "Dimensions",
+                "value": {
+                  "xAxis": {
+                    "name": "TimeGenerated",
+                    "type": "datetime"
+                  },
+                  "yAxis": [
+                    {
+                      "name": "count_",
+                      "type": "long"
+                    }
+                  ],
+                  "splitBy": [],
+                  "aggregation": "Sum"
+                }
+              },
+              {
+                "name": "Version",
+                "value": "1.0"
+              },
+              {
+                "name": "PartId",
+                "value": "44b942d7-0dd3-4979-b729-05f2d156001f"
+              },
+              {
+                "name": "PartTitle",
+                "value": "Analytics"
+              },
+              {
+                "name": "PartSubTitle",
+                "value": "${azurerm_log_analytics_workspace.cluster.name}"
+              },
+              {
+                "name": "resourceTypeMode",
+                "value": "workspace"
+              },
+              {
+                "name": "ControlType",
+                "value": "AnalyticsGrid"
+              },
+              {
+                "name": "SpecificChart",
+                "value": "Line"
+              },
+              {
+                "name": "DashboardId",
+                "isOptional": true
+              }
+            ],
+            "type": "Extension/AppInsightsExtension/PartType/AnalyticsPart",
+            "settings": {
+              "content": {
+                "PartTitle": "Doc index updater server errors list",
+                "PartSubTitle": "${azurerm_log_analytics_workspace.cluster.name}"
+              }
+            },
+            "asset": {
+              "idInputName": "ComponentId",
+              "type": "ApplicationInsights"
+            }
+          }
+        }
       }
     }
   },
@@ -704,12 +966,16 @@ resource "azurerm_dashboard" "doc-index-updater-dashboard" {
               "value": "Past hour"
             },
             "filteredPartIds": [
-              "StartboardPart-AnalyticsPart-356f6966-dcff-41c3-8956-172e3e39d006",
-              "StartboardPart-AnalyticsPart-356f6966-dcff-41c3-8956-172e3e39d008",
-              "StartboardPart-AnalyticsPart-356f6966-dcff-41c3-8956-172e3e39d00a",
-              "StartboardPart-AnalyticsPart-356f6966-dcff-41c3-8956-172e3e39d00c",
-              "StartboardPart-AnalyticsPart-356f6966-dcff-41c3-8956-172e3e39d00e",
-              "StartboardPart-AnalyticsPart-356f6966-dcff-41c3-8956-172e3e39d010"
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f428f",
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f4291",
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f4293",
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f4295",
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f4297",
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f4299",
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f429b",
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f429d",
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f429f",
+              "StartboardPart-AnalyticsPart-ffee39a5-76e8-4033-800a-344cb47f42a1"
             ]
           }
         }
