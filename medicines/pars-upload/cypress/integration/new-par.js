3d466fb617695298ce283e206c8c2fd602540857
Tim Lee <tim.lee@red-badger.com>
2020-09-02 12:41:15 +0100

    Use substance instead of substances

--- medicines/pars-upload/cypress/integration/new-par.js
+++ medicines/pars-upload/cypress/integration/new-par.js
@@ -64,7 +64,7 @@ describe('New PARs upload', () => {
       brand: 'Ibuprofen pills',
       strength: 'Really powerful stuff',
       doseForm: 'some form',
-      substances: 'Ibuprofen',
+      substance: 'Ibuprofen',
       licence: { type: 'THR', part_one: '12345', part_two: '6789' },
     }
     let uploadPageTitle = 'New Public Assessment Report'
