3db4cf008af40bdcf208aaa39164636c98f510b3
Tim Lee <tim.lee@red-badger.com>
2020-10-09 17:00:04 +0100

    Fix breaking change introduced by sass-loader update

--- medicines/pars-upload/next.config.js
+++ medicines/pars-upload/next.config.js
@@ -1,5 +1,22 @@
-const withPlugins = require('next-compose-plugins')
+const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 
-module.exports = withPlugins([
-  // your other plugins here
-])
+// This webpack configuration gets around problem of importing woff and woff2 files for the govuk-frontend package
+// As suggested: https://dev.to/harveyjones282/the-simplest-way-to-configure-next-js-with-sass-3en
+module.exports = withCSS(
+  withSass({
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+          },
+        },
+      })
+
+      return config
+    },
+  })
+)
