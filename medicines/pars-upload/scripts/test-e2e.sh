1cb89b68168d355befeefe7c51c90bec4d0e2883
Tim Lee <tim.lee@red-badger.com>
2020-10-28 11:06:16 +0000

    Run cypress tests on built version instead of dev version

--- medicines/pars-upload/scripts/test-e2e.sh
+++ medicines/pars-upload/scripts/test-e2e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 port=3000
-cross-env NEXT_PUBLIC_DISABLE_AUTH=true yarn dev -- -p $port & 
+cross-env NEXT_PUBLIC_DISABLE_AUTH=true yarn build && yarn start -p $port &
 wait-on http://localhost:$port
 server_pid=$!
 yarn cypress run --env PARS_UPLOAD_URL=$PARS_UPLOAD_URL --config baseUrl=http://localhost:$port
