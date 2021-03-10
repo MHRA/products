b8e15bfd0307d7b7ff7bbc8411005b10d18ffa23
Tim Lee <tim.lee@red-badger.com>
2021-03-10 15:32:28 +0000

    Remove @zeit/sass and @zeit/css

--- medicines/pars-upload/next.config.js
+++ medicines/pars-upload/next.config.js
@@ -1,22 +1,17 @@
-const withSass = require('@zeit/next-sass')
-const withCSS = require('@zeit/next-css')
-
 // This webpack configuration gets around problem of importing woff and woff2 files for the govuk-frontend package
 // As suggested: https://dev.to/harveyjones282/the-simplest-way-to-configure-next-js-with-sass-3en
-module.exports = withCSS(
-  withSass({
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-          },
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
         },
-      })
+      },
+    })
 
-      return config
-    },
-  })
-)
+    return config
+  },
+}
