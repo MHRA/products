88ce1ac87cdf538d7c579614bd067072c1051c54
Tim Lee <tim.lee@red-badger.com>
2022-05-25 14:02:12 +0100

    Replace fixture syntax

--- medicines/web/cypress/integration/graphql_tests.js
+++ medicines/web/cypress/integration/graphql_tests.js
@@ -5,23 +5,23 @@
 
 // Some code to do this is adapted from https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/stubbing-spying__window-fetch/cypress/integration/polyfill-fetch-from-tests-spec.js
 
-let polyfill;
+// let polyfill;
 
-// grab fetch polyfill from remote URL, could be also from a local package
-before(() => {
-  const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
+// // grab fetch polyfill from remote URL, could be also from a local package
+// before(() => {
+//   const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
 
-  cy.request(polyfillUrl).then((response) => {
-    polyfill = response.body;
-  });
-});
+//   cy.request(polyfillUrl).then((response) => {
+//     polyfill = response.body;
+//   });
+// });
 
 Cypress.on('window:before:load', (win) => {
-  delete win.fetch;
+  // delete win.fetch;
   // since the application code does not ship with a polyfill
   // load a polyfilled "fetch" from the test
-  win.eval(polyfill);
-  win.fetch = win.unfetch;
+  // win.eval(polyfill);
+  // win.fetch = win.unfetch;
 
   // Clear out session storage so that the disclaimer is always presented.
   win.sessionStorage.clear();
@@ -30,13 +30,12 @@ Cypress.on('window:before:load', (win) => {
 const graphQlUrl = Cypress.env('GRAPHQL_URL');
 
 const mockParacetamolResultsForGraphQl = () =>
-  cy.route('POST', graphQlUrl, 'fixture:graphql-search-results.json');
+  cy.intercept('POST', graphQlUrl, { fixture: 'graphql-search-results' });
 
 const longerTimeout = 20000;
 
 describe('Search using GraphQl', function () {
-  it('can search for Paracetamol', function () {
-    cy.server();
+  it.only('can search for Paracetamol', function () {
     mockParacetamolResultsForGraphQl();
     cy.visit('/search?search=paracetamol&page=1');
     cy.contains('I have read and understand the disclaimer', {
@@ -49,20 +48,19 @@ describe('Search using GraphQl', function () {
 
 describe('A-Z Index', function () {
   it('can navigate to Paracetamol Tablets with GraphQL feature on', function () {
-    cy.server();
     // Mock out GraphQL response.
-    cy.route('POST', graphQlUrl, 'fixture:graphql-substances-index.json');
+    cy.intercept('POST', graphQlUrl, 'fixture:graphql-substances-index.json');
 
     cy.visit('/');
     cy.get('nav').contains('P').click();
     cy.contains('PARACETAMOL');
 
-    cy.route('POST', graphQlUrl, 'fixture:graphql-products-index.json');
+    cy.intercept('POST', graphQlUrl, 'fixture:graphql-products-index.json');
 
     cy.contains('PARACETAMOL').click();
     cy.contains('PARACETAMOL 500MG CAPSULES');
 
-    cy.route('POST', graphQlUrl, 'fixture:graphql-product-results.json');
+    cy.intercept('POST', graphQlUrl, 'fixture:graphql-product-results.json');
 
     cy.contains('PARACETAMOL 500MG CAPSULES').click();
     cy.contains('I have read and understand the disclaimer', {
@@ -74,11 +72,10 @@ describe('A-Z Index', function () {
 });
 
 const mockParacetamolResultsForMedicineLevelsInPregnancyGraphQl = () =>
-  cy.route('POST', graphQlUrl, 'fixture:graphql-search-results-bmgf.json');
+  cy.intercept('POST', graphQlUrl, 'fixture:graphql-search-results-bmgf.json');
 
 describe('Search medicine levels in pregnancy docs using GraphQl', function () {
   it('can search for Paracetamol', function () {
-    cy.server();
     mockParacetamolResultsForMedicineLevelsInPregnancyGraphQl();
     cy.visit('/medicine-levels-in-pregnancy/search?search=paracetamol&page=1');
     cy.get(
@@ -89,15 +86,22 @@ describe('Search medicine levels in pregnancy docs using GraphQl', function () {
 
 describe('A-Z Index for medicine levels in pregnancy', function () {
   it('can navigate to Paracetamol Tablets with GraphQL feature on', function () {
-    cy.server();
     // Mock out GraphQL response.
-    cy.route('POST', graphQlUrl, 'fixture:graphql-substances-index-bmgf.json');
+    cy.intercept(
+      'POST',
+      graphQlUrl,
+      'fixture:graphql-substances-index-bmgf.json',
+    );
 
     cy.visit('/medicine-levels-in-pregnancy');
     cy.get('nav').contains('P').click();
     cy.contains('PARACETAMOL');
 
-    cy.route('POST', graphQlUrl, 'fixture:graphql-substance-results-bmgf.json');
+    cy.intercept(
+      'POST',
+      graphQlUrl,
+      'fixture:graphql-substance-results-bmgf.json',
+    );
 
     cy.contains('PARACETAMOL').click();
 
